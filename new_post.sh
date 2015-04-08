7e8eb53cc1cd0f7a74bc97afdc00f64532d250f3
Nicolas Hoizey <nicolas@hoizey.com>
2015-04-08 02:43:05 +0200

    New post script

---
+++ new_post.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]; then
+echo "Usage: provide a title argument."
+exit -1
+else
+title="$@"
+fi
+
+d=$(date +"%Y-%m-%d")
+
+slug=$(echo $title | tr '[:upper:]' '[:lower:]' | tr '[:space:]' '-' | tr '[:punct:]' '-')
+slug="${slug:0:${#slug}-1}"
+post="./_drafts/$d-$slug.md"
+
+body=""
+read -d '' body <<EOF
+---
+title:			$title
+lang:				fr
+categories:
+-
+-
+---
+
+EOF
+
+echo "$body" > $post
+/Applications/MacDown.app/Contents/MacOS/MacDown $post &
