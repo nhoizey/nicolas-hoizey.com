f41cf837194645d61dd774841bf9acafa4a31ca6
Nicolas Hoizey <nicolas@hoizey.com>
2015-04-22 23:29:36 +0200

    Create a draft and the folder for source images

--- _new_post.sh
+++ _new_post.sh
@@ -1,27 +1,30 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 if [ -z "$1" ]; then
-echo "Usage: provide a title argument."
-exit -1
+	echo "Usage: provide a title argument."
+	exit -1
 else
-title="$@"
+	title="$@"
 fi
 
+y=$(date +"%Y")
 d=$(date +"%Y-%m-%d")
-
 slug=$(echo $title | tr '[:upper:]' '[:lower:]' | tr '[:space:]' '-' | tr '[:punct:]' '-')
 slug="${slug:0:${#slug}-1}"
 post="./_drafts/$d-$slug.md"
+assets="./_assets/fullsize-images/$y/$d-$slug/"
 
 body=""
 read -d '' body <<EOF
 ---
 title:			$title
 lang:				fr
-tags: 			[ , ]
+tags:       [ ]
 ---
 
 EOF
 
 echo "$body" > $post
+mkdir "$assets"
+
 /Applications/MacDown.app/Contents/MacOS/MacDown $post &
