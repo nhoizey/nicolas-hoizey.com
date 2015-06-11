c7d3e957e33ec8eb9c89022c83fd11b661903375
Nicolas Hoizey <nicolas@hoizey.com>
2015-06-12 01:44:59 +0200

    No limit

--- _serve.sh
+++ _serve.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-jekyll s --config _config.yml,_config_credentials.yml --limit_posts 15
+#jekyll s --config _config.yml,_config_credentials.yml --limit_posts 15
+jekyll s --config _config.yml,_config_credentials.yml
