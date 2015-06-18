2a17f8557c50564f7d1ca3e4b3f010aaa97ff50c
Nicolas Hoizey <nicolas@hoizey.com>
2015-06-18 15:31:20 +0200

    Fixes thanks to http://www.shellcheck.net/

--- _new_post.sh
+++ _new_post.sh
@@ -8,17 +8,17 @@ else
 fi
 
 # Slugify, inspired by https://github.com/benlinton/bash-slugify/blob/master/slugify
-slug=$(echo "$title" | tr [:upper:] [:lower:])
-slug=$(echo "$slug" | sed 'y/āáǎàēéěèīíǐìōóǒòūúǔùǖǘǚǜĀÁǍÀĒÉĚÈĪÍǏÌŌÓǑÒŪÚǓÙǕǗǙǛ/aaaaeeeeiiiioooouuuuüüüüAAAAEEEEIIIIOOOOUUUUÜÜÜÜ/')
-slug=$(echo "$slug" | tr [:punct:] " ")
+slug=$(echo "$title" | tr "[:upper:]" "[:lower:]")
+slug=$(echo "$slug" | sed "y/āáǎàēéěèīíǐìōóǒòūúǔùǖǘǚǜĀÁǍÀĒÉĚÈĪÍǏÌŌÓǑÒŪÚǓÙǕǗǙǛ/aaaaeeeeiiiioooouuuuüüüüAAAAEEEEIIIIOOOOUUUUÜÜÜÜ/")
+slug=$(echo "$slug" | tr "[:punct:]" " ")
 slug=$(echo "$slug" | tr _ " ")
 slug=$(echo "$slug" | tr - " ")
 slug=$(echo "$slug" | tr -s " ")
-slug=$(echo "$slug" | tr [:space:] "-")
+slug=$(echo "$slug" | tr "[:space:]" "-")
 slug="${slug:0:${#slug}-1}"
 
 folder="./_drafts/$slug"
-mkdir -p $folder
+mkdir -p "$folder"
 
 post="$folder/$slug.md"
 
@@ -32,6 +32,6 @@ tags:  [ ]
 
 EOF
 
-echo "$body" > $post
+echo "$body" > "$post"
 
-/Applications/MacDown.app/Contents/MacOS/MacDown $post &
+/Applications/MacDown.app/Contents/MacOS/MacDown "$post" &
