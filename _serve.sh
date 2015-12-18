d35665c03d1fe48a7c1963647d6f9c1eccffcfde
Nicolas Hoizey <nicolas@hoizey.com>
2015-12-18 23:13:10 +0100

    Run jekyll with bundle

--- _serve.sh
+++ _serve.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-#jekyll s --config _config.yml,_config_credentials.yml --limit_posts 15
-jekyll s --config _config.yml,_config_credentials.yml
+bundle exec jekyll serve --config _config.yml,_config_credentials.yml --limit_posts 15
+#bundle exec jekyll serve --config _config.yml,_config_credentials.yml
