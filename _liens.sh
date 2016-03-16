66e99c42447ac85f74d0413da80bb292dc7b3ebb
Nicolas Hoizey <nicolas@hoizey.com>
2016-03-16 15:44:56 +0100

    Getting last Pinboard bookmarks

--- _liens.sh
+++ _liens.sh
@@ -1,13 +1,37 @@
 #!/bin/sh
 
-y=$(date +"%Y")
-m=$(date +"%m")
-d=$(date +"%d")
+content="`/usr/local/bin/pinpress pins last -n 50 | grep -v '# No data matching your arguments...'`"
+lines=`echo "$content" | wc -l`
 
-# Create post parent folder
-dest_parent="./_posts/$y/$m"
-mkdir -p $dest_parent
+if [ $lines -gt 1 ]
+then
+  y=$(date +"%Y")
+  m=$(date +"%m")
+  d=$(date +"%d")
 
-# Create links post
-dest="$dest_parent/$y-$m-$d-quelques-liens-a-visiter"
-/usr/local/bin/pinpress pins last | grep -v "# No data matching your arguments..." > $dest
+  dest_parent="./_drafts/en-vrac"
+  dest_file="$dest_parent/en-vrac.md"
+
+  if [ ! -f $dest_file ]
+  then
+    # Create post parent folder
+    mkdir -p $dest_parent
+
+    # Initiate file with YAML front matter
+    cat <<EOYFT >> $dest_file
+---
+title: En vrac
+lang:  fr
+tags:  [liens]
+---
+
+Assez régulièrement, je vais essayer de vous faire profiter des liens vers les sites, articles ou projets les plus intéressants que je trouve au cours de mes pérégrinations sur le Web.
+
+EOYFT
+    fi
+
+  echo "$content" >> $dest_file
+  /Applications/MacDown.app/Contents/MacOS/MacDown "$dest_file" &
+else
+  echo "No new link"
+fi
