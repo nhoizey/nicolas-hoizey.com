6f17743f120221596552cb5d343e00ce687a5158
Nicolas Hoizey <nicolas@hoizey.com>
2016-03-22 19:42:24 +0100

    Use a SVG link icon for deep links

--- _assets/javascripts/behaviors.js
+++ _assets/javascripts/behaviors.js
@@ -39,9 +39,9 @@
 
   for (i = 0; i < nb; ++i) {
     if (undefined !== list[i].id) {
-      list[i].innerHTML += '&nbsp;<a class="deeplink" href="#' + list[i].id + '">#︎</a>';
+      list[i].innerHTML += '&nbsp;<a class="deeplink" href="#' + list[i].id + '"><svg class="icon"><use xlink:href="#symbol-link" /></svg>︎</a>';
     } else {
-      list[i].innerHTML += '&nbsp;<a class="deeplink" href="#' + i + '">#</a>';
+      list[i].innerHTML += '&nbsp;<a class="deeplink" href="#' + i + '"><svg class="icon"><use xlink:href="#symbol-link" /></svg></a>';
     }
   }
 }(this));
