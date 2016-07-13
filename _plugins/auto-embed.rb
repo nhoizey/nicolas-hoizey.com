4d29fd3f0e4bbf271e5df131c22babbd9d1f1efb
Nicolas Hoizey <nicolas@hoizey.com>
2016-07-13 02:54:40 +0200

    Use HTTPS URLs

--- _plugins/auto-embed.rb
+++ _plugins/auto-embed.rb
@@ -13,11 +13,11 @@ module Jekyll
 
       def convert(content)
         youtube_regex = /\nhttps?:\/\/www\.youtube\.com\/watch\?v=(?<youtube_id>[^\n]+)\n/
-        youtube_html = '<div class="video-wrapper"><iframe class="youtube" type="text/html" width="800" height="490" src="http://www.youtube.com/embed/\k<youtube_id>?enablejsapi=1&origin=https://nicolas-hoizey.com" frameborder="0"></iframe></div>'
+        youtube_html = '<div class="video-wrapper"><iframe class="youtube" type="text/html" width="800" height="490" src="https://www.youtube.com/embed/\k<youtube_id>?enablejsapi=1&origin=https://nicolas-hoizey.com" frameborder="0"></iframe></div>'
         content = content.gsub(youtube_regex, youtube_html)
 
         vimeo_regex = /\nhttps:\/\/vimeo\.com\/(?<vimeo_id>[0-9]+)\n/
-        vimeo_html = '<div class="video-wrapper"><iframe src="http://player.vimeo.com/video/\k<vimeo_id>" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>'
+        vimeo_html = '<div class="video-wrapper"><iframe src="https://player.vimeo.com/video/\k<vimeo_id>" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>'
         content = content.gsub(vimeo_regex, vimeo_html)
 
         content
