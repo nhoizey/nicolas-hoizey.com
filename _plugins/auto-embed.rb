cc17d62caa3a02919881a427cf38702ccedb6dff
Nicolas Hoizey <nicolas@hoizey.com>
2016-07-21 00:34:47 +0200

    Add a class for the service name

--- _plugins/auto-embed.rb
+++ _plugins/auto-embed.rb
@@ -13,11 +13,11 @@ module Jekyll
 
       def convert(content)
         youtube_regex = /\nhttps?:\/\/www\.youtube\.com\/watch\?v=(?<youtube_id>[^\n]+)\n/
-        youtube_html = '<div class="video-wrapper"><iframe class="youtube" type="text/html" width="800" height="490" src="https://www.youtube.com/embed/\k<youtube_id>?enablejsapi=1&origin=https://nicolas-hoizey.com" frameborder="0" sandbox=”allow-scripts"></iframe></div>'
+        youtube_html = '<div class="video-wrapper youtube"><iframe class="youtube" type="text/html" width="800" height="490" src="https://www.youtube.com/embed/\k<youtube_id>?enablejsapi=1&origin=https://nicolas-hoizey.com" frameborder="0" sandbox=”allow-scripts"></iframe></div>'
         content = content.gsub(youtube_regex, youtube_html)
 
         vimeo_regex = /\nhttps:\/\/vimeo\.com\/(?<vimeo_id>[0-9]+)\n/
-        vimeo_html = '<div class="video-wrapper"><iframe src="https://player.vimeo.com/video/\k<vimeo_id>" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen sandbox=”allow-scripts"></iframe></div>'
+        vimeo_html = '<div class="video-wrapper vimeo"><iframe src="https://player.vimeo.com/video/\k<vimeo_id>" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen sandbox=”allow-scripts"></iframe></div>'
         content = content.gsub(vimeo_regex, vimeo_html)
 
         content
