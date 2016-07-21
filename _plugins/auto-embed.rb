f5fa9fd690b43a5962e87edabc6ca729a6edef25
Nicolas Hoizey <nicolas@hoizey.com>
2016-07-21 13:04:27 +0200

    Bad quotes

--- _plugins/auto-embed.rb
+++ _plugins/auto-embed.rb
@@ -12,12 +12,14 @@ module Jekyll
       end
 
       def convert(content)
+        # Youtube
         youtube_regex = /\nhttps?:\/\/www\.youtube\.com\/watch\?v=(?<youtube_id>[^\n]+)\n/
-        youtube_html = '<div class="video-wrapper youtube"><iframe class="youtube" type="text/html" width="800" height="490" src="https://www.youtube.com/embed/\k<youtube_id>?enablejsapi=1&origin=https://nicolas-hoizey.com" frameborder="0" sandbox=”allow-scripts"></iframe></div>'
+        youtube_html = '<div class="responsive-wrapper youtube"><iframe class="youtube" type="text/html" width="800" height="490" src="https://www.youtube.com/embed/\k<youtube_id>?enablejsapi=1&origin=https://nicolas-hoizey.com" frameborder="0" sandbox="allow-scripts"></iframe></div>'
         content = content.gsub(youtube_regex, youtube_html)
 
+        # Vimeo
         vimeo_regex = /\nhttps:\/\/vimeo\.com\/(?<vimeo_id>[0-9]+)\n/
-        vimeo_html = '<div class="video-wrapper vimeo"><iframe src="https://player.vimeo.com/video/\k<vimeo_id>" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen sandbox=”allow-scripts"></iframe></div>'
+        vimeo_html = '<div class="responsive-wrapper vimeo"><iframe src="https://player.vimeo.com/video/\k<vimeo_id>" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen sandbox="allow-scripts"></iframe></div>'
         content = content.gsub(vimeo_regex, vimeo_html)
 
         content
