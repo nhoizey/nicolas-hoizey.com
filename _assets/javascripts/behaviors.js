076e5b1f5086ae0d65cc026d2c0b24cdb42b469f
Nicolas Hoizey <nicolas@hoizey.com>
2016-12-23 18:01:47 +0100

    Cleaning Service Worker installation

--- _assets/javascripts/behaviors.js
+++ _assets/javascripts/behaviors.js
@@ -24,10 +24,8 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js', {scope: '/'})
     .then(function(reg) {
-      // registration worked
-      console.log('Registration succeeded. Scope is ' + reg.scope);
     }).catch(function(error) {
       // registration failed
-      console.log('Registration failed with ' + error);
+      console.error('Registration failed with ' + error);
     });
 }
