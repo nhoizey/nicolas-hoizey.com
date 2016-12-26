737121d0e6ea16401ce0800f39a66a1dd5806e99
Nicolas Hoizey <nicolas@hoizey.com>
2016-12-26 23:57:57 +0100

    Force update on activation

--- sw.js
+++ sw.js
@@ -25,6 +25,11 @@ self.addEventListener('install', event => {
   );
 });
 
+self.addEventListener('activate', event => {
+  // https://davidwalsh.name/service-worker-claim
+  return self.clients.claim();
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     fetch(event.request).catch(() => {
