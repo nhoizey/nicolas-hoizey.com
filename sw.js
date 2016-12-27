ddf6da2967a63b2bdbf4aba51550dadcb61db558
Nicolas Hoizey <nicolas@hoizey.com>
2016-12-27 22:19:21 +0100

    Full rewrite based on Jeremy Keith's SW

--- sw.js
+++ sw.js
@@ -1,80 +1,150 @@
 ---
 ---
+'use strict';
 
-// https://remysharp.com/2016/03/22/the-copy--paste-guide-to-your-first-service-worker
-// https://jakearchibald.com/2014/offline-cookbook/#network-falling-back-to-cache
+// Inspirations:
+// - Jake Archibald's Offline Cookbook: https://jakearchibald.com/2014/offline-cookbook/
+// - Jeremy Keith's Service Worker: https://adactio.com/journal/9775
 
-const CACHE = 'v1::static';
+const version = '0.1';
+const staticCacheName = `v${version}::static`;
+const pagesCacheName = `v${version}::pages`;
+const imagesCacheName = `v${version}::images`;
 
-var urlsToCache = [
-      '/',
-      '/a-propos/de-moi.html',
-      '/a-propos/du-site.html',
-      {% for post in site.posts limit:7 %}
-      '{{ post.url }}',
-      {% endfor %}
-      '{% asset_path "non-critical-styles" %}',
-      '/offline.html',
-    ];
+const offlinePages = [
+  '/',
+  {% for post in site.posts limit:1 %}
+  '{{ post.url }}',
+  {% endfor %}
+];
+
+const offlineStatusPage = '/offline.html'
+
+function updateStaticCache() {
+  return caches.open(staticCacheName)
+    .then(cache => {
+      // These items won't block the installation of the Service Worker
+      cache.addAll(offlinePages);
+      // These items must be cached for the Service Worker to complete installation
+      return cache.addAll([
+        offlineStatusPage,
+        '{% asset_path "non-critical-styles" %}',
+      ]);
+  });
+}
+
+function stashInCache(cacheName, request, response) {
+  caches.open(cacheName)
+    .then(cache => cache.put(request, response));
+}
+
+// Limit the number of items in a specified cache.
+function trimCache(cacheName, maxItems) {
+  caches.open(cacheName)
+    .then(cache => {
+      cache.keys()
+        .then(keys => {
+          if (keys.length > maxItems) {
+            cache.delete(keys[0])
+              .then(trimCache(cacheName, maxItems));
+          }
+        });
+    });
+}
+
+// Remove caches whose name is no longer valid
+function clearOldCaches() {
+  return caches.keys()
+    .then(keys => {
+      return Promise.all(keys
+        .filter(key => key.indexOf(`v${version}::`) !== 0)
+        .map(key => caches.delete(key))
+      );
+    });
+}
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => {
-      return cache.addAll(urlsToCache).then(() => self.skipWaiting());
-    })
+  event.waitUntil(updateStaticCache()
+    .then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener('activate', event => {
-  // https://davidwalsh.name/service-worker-claim
-  return self.clients.claim();
+  event.waitUntil(clearOldCaches()
+    .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('message', event => {
+  if (event.data.command == 'trimCaches') {
+    trimCache(pagesCacheName, 35);
+    trimCache(imagesCacheName, 20);
+  }
 });
 
 self.addEventListener('fetch', event => {
-  var requestURL = new URL(event.request.url);
-  // local URL
-  if (requestURL.origin == location.origin) {
+  let request = event.request;
+  let url = new URL(request.url);
 
-    // post
-    if (/^\/([0-9]{4}\/|a-propos)/.test(requestURL.pathname)) {
-      event.respondWith(
-        caches.match(event.request).then(response => {
-          // if in cache, use it
-          // else, fetch and put in the cache
-          return response || fetch(event.request).then(response => {
-            cache.put(event.request, response.clone());
-            return response;
-          });
-        }).catch(function() {
-          // not in cache and offline fallback
-          return caches.match('/offline.html');
-        })
-      );
-      return;
-    } else {
-      // local CSS
-      if (/^\/assets\/non-critical-styles/.test(requestURL.pathname)) {
-        event.respondWith(
-          caches.match(event.request).then(response => {
-            return response || fetch(event.request).then(response => {
-              cache.put(event.request, response.clone());
-              return response;
-            });
-          })
-        );
-    }
+  // Ignore requests to some directories
+  // if (request.url.indexOf('/mint') !== -1 || request.url.indexOf('/cms') !== -1) {
+  //     return;
+  // }
+
+  // Ignore non-GET requests
+  if (request.method !== 'GET') {
+    return;
   }
 
-  // Cloudinary images
-  if (requestURL.hostname == 'res.cloudinary.com') {
+  // For HTML requests:
+  // - try the network first,
+  // - fall back to the cache,
+  // - finally the offline page
+  if (request.headers.get('Accept').indexOf('text/html') !== -1) {
     event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request).then(response => {
-          cache.put(event.request, response.clone());
+      fetch(request)
+        .then(response => {
+          // NETWORK
+          // Stash a copy of this page in the pages cache
+          let copy = response.clone();
+          if (offlinePages.includes(url.pathname) || offlinePages.includes(url.pathname + '/')) {
+            stashInCache(staticCacheName, request, copy);
+          } else {
+            stashInCache(pagesCacheName, request, copy);
+          }
           return response;
-        });
-      })
+        })
+        .catch(() => {
+          // CACHE or FALLBACK
+          return caches.match(request)
+            .then(response => response || caches.match(offlineStatusPage) );
+        })
     );
+    return;
   }
 
+  // For non-HTML requests, look in the cache first, fall back to the network
+  event.respondWith(
+    caches.match(request)
+      .then(response => {
+        // CACHE
+        return response || fetch(request)
+          .then(response => {
+            // NETWORK
+            // If the request is for an image, stash a copy of this image in the images cache
+            if (request.headers.get('Accept').indexOf('image') !== -1) {
+              let copy = response.clone();
+              stashInCache(imagesCacheName, request, copy);
+            }
+            return response;
+          })
+          .catch(() => {
+            // OFFLINE
+            // If the request is for an image, show an offline placeholder
+            if (request.headers.get('Accept').indexOf('image') !== -1) {
+              return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', {headers: {'Content-Type': 'image/svg+xml'}});
+            }
+          });
+      })
+  );
 });
