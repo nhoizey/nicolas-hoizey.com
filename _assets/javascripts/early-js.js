952afc3bb1ae33d09dc7e8a760a61a0349ea3fc6
Nicolas Hoizey <nicolas@hoizey.com>
2017-11-29 13:40:47 +0100

    Prettier

--- _assets/javascripts/early-js.js
+++ _assets/javascripts/early-js.js
@@ -1,9 +1,21 @@
+/*****************************************************************
+ * Analytics
+ * ****************************************************************/
+
 // get device pixel ratio in dppx
 // https://github.com/ryanve/res/blob/master/src/index.js
-var screen_density = (typeof window == 'undefined') ? 0 : (+window.devicePixelRatio || Math.sqrt(screen.deviceXDPI*screen.deviceYDPI)/96 || 0);
+var screen_density =
+  typeof window == 'undefined'
+    ? 0
+    : +window.devicePixelRatio ||
+      Math.sqrt(screen.deviceXDPI * screen.deviceYDPI) / 96 ||
+      0
 // keep only 3 decimals: http://jsfiddle.net/AsRqx/
-screen_density = +(Math.round(screen_density + "e+3")  + "e-3");
+screen_density = +(Math.round(screen_density + 'e+3') + 'e-3')
 
 // get viewport width
 // http://stackoverflow.com/a/8876069/717195
-var viewport_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+var viewport_width = Math.max(
+  document.documentElement.clientWidth,
+  window.innerWidth || 0,
+)
