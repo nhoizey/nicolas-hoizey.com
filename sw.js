754ae9fff2ddc591112b2ee647501dc0aa125a36
Nicolas Hoizey <nicolas@hoizey.com>
2018-03-31 23:36:34 +0200

    Update Service Worker version

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
 // - Jake Archibald's Offline Cookbook: https://jakearchibald.com/2014/offline-cookbook/
 // - Jeremy Keith's Service Worker: https://adactio.com/journal/9775
 
-const version = '0.12';
+const version = '0.13';
 const staticCacheName = `v${version}::static`;
 const pagesCacheName = `v${version}::pages`;
 const imagesCacheName = `v${version}::images`;
