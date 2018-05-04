c5348b113cebd570368a0e541f409e1e76cbba8c
Nicolas Hoizey <nicolas@hoizey.com>
2018-05-05 01:00:23 +0200

    Strikethrough text using unicode

---
+++ _plugins/strikethrough.rb
@@ -0,0 +1,21 @@
+require 'nokogiri'
+
+module Jekyll
+  module StrikethroughFilter
+
+    def strikethrough(raw)
+      document = Nokogiri::HTML.fragment(raw.encode('UTF-8', :invalid => :replace, :undef => :replace, :replace => ''))
+
+      for block in ['del'] do
+        document.css(block).each do |element|
+          element.content = element.content.gsub(/(.)/) {|c| "#{c}\u0336"}
+        end
+      end
+
+      document.inner_html
+
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::StrikethroughFilter)
