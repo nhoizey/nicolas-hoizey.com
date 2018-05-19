f576ecbbe727f41d7e1ca0fb039b155e20b86b5f
Nicolas Hoizey <nicolas@hoizey.com>
2018-05-19 20:08:55 +0200

    Don't index comments in Algolia

--- _plugins/algolia_hooks.rb
+++ _plugins/algolia_hooks.rb
@@ -1,3 +1,14 @@
+module Jekyll
+  module Algolia
+    module Hooks
+      def self.before_indexing_each(record, node, context)
+        record[:comments] = nil
+        record
+      end
+    end
+  end
+end
+
 # https://community.algolia.com/jekyll-algolia/hooks.html#should-be-excluded
 module Jekyll
   module Algolia
