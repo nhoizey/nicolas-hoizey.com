3dbdec39114d9a83faa2f55e92b37d80a0b5e035
Nicolas Hoizey <nicolas@hoizey.com>
2018-07-03 11:56:02 +0200

    Get first image src

---
+++ _plugins/first_image_url.rb
@@ -0,0 +1,33 @@
+require 'nokogiri'
+
+module Jekyll
+  module FirstImageUrlFilter
+
+    def first_image_url(content)
+      return content if content.nil?
+
+      doc = Nokogiri::HTML.fragment(content.encode('UTF-8', :invalid => :replace, :undef => :replace, :replace => ''))
+
+      image = doc.at_css('img').to_h
+
+      if image['srcset'] then
+        src = ''
+        src_width = 0
+        image['srcset'].split(',').each do |src_fragment|
+          src_fragment.gsub!(/\s*([^\s]+)\s+([0-9]+)(w|x)/) { |url, size|
+            if size.to_i > src_width then
+              src = url
+              src_width = size.to_i
+            end
+          }
+        end
+      else
+        src = image['src']
+      end
+
+      src
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::FirstImageUrlFilter)
