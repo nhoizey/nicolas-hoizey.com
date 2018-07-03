4bde0efb47fdea7080ac6d82b27c426838858e40
Nicolas Hoizey <nicolas@hoizey.com>
2018-07-04 00:55:50 +0200

    Map/Reduce FTW!

--- _plugins/first_image_url.rb
+++ _plugins/first_image_url.rb
@@ -4,25 +4,19 @@ module Jekyll
   module FirstImageUrlFilter
 
     def first_image_url(content)
-      return content if content.nil?
+      return '' if content.nil?
 
       doc = Nokogiri::HTML.fragment(content.encode('UTF-8', :invalid => :replace, :undef => :replace, :replace => ''))
 
-      image = doc.at_css('img').to_h
+      image_elements = doc.css('img')
+
+      return '' if image_elements.empty?
 
       src = ''
-
+      image = image_elements.first.to_h
       if image['srcset'] then
-        src_width = 0
-        image['srcset'].split(',').each do |src_fragment|
-          src_fragment.gsub!(/\s*([^\s]+)\s+([0-9]+)(w|x)/) { |url, size|
-            if size.to_i > src_width then
-              src = url
-              src_width = size.to_i
-            end
-          }
-        end
-      else
+        src = image['srcset'].scan(/([^, ][^ ]+)\s+([0-9]+)w/).map{ |url, size| { 'url' => url.strip, 'size' => size.to_i } }.reduce({ 'url' => '', 'size' => 0 }){ |current, new| current = new if new['size'] > current['size'] }['url']
+      elsif image['src'] then
         src = image['src']
       end
 
