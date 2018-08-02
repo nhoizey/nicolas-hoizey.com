4a421348ae5c9efd316cb85fa6769e6f12c1c0d5
Nicolas Hoizey <nicolas@hoizey.com>
2018-08-02 19:09:41 +0200

    Fix a content type typo

--- _plugins/giphy.rb
+++ _plugins/giphy.rb
@@ -41,7 +41,7 @@ class Giphy < Liquid::Tag
     cloudinaryPrefix = "https://res.cloudinary.com/#{cloudname}/image/fetch"
     giphyImage = "https://media.giphy.com/media/#{@id}/giphy.gif"
     mp4Source = "<source src=\"#{cloudinaryPrefix}/f_mp4/#{giphyImage}\" type=\"video/mp4\">"
-    webmSource = "<source src=\"#{cloudinaryPrefix}/f_webm/#{giphyImage}\" type=\"video/mp4\">"
+    webmSource = "<source src=\"#{cloudinaryPrefix}/f_webm/#{giphyImage}\" type=\"video/webm\">"
     fallback = "<p>Your browser doesn't support video. See <a href=\"#{giphyImage}\">the animated GIF</a>.</p>"
     videoTag = "<video autoplay loop muted playsinline>#{mp4Source}#{webmSource}#{fallback}</video>"
 
