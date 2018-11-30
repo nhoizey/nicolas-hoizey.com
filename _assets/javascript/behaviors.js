2743239853e466d08dadc48319271813b803230c
Nicolas Hoizey <nicolas@hoizey.com>
2018-11-30 16:31:02 +0100

    Generate deep link anchors only when there is an id

--- _assets/javascript/behaviors.js
+++ _assets/javascript/behaviors.js
@@ -16,22 +16,15 @@
 (function(w) {
   var i,
     list = w.document.querySelectorAll(
-      'article.main h2, article.main h3, article.main h4, article.main h5, article.main h6',
+      'article.main h2[id], article.main h3[id], article.main h4[id], article.main h5[id], article.main h6[id]',
     ),
     nb = list.length;
 
   for (i = 0; i < nb; ++i) {
-    if (undefined !== list[i].id) {
-      list[i].innerHTML +=
-        '&nbsp;<a class="deeplink" href="#' +
-        list[i].id +
-        '"><svg><use xlink:href="#symbol-link" /></svg></a>';
-    } else {
-      list[i].innerHTML +=
-        '&nbsp;<a class="deeplink" href="#' +
-        i +
-        '"><svg><use xlink:href="#symbol-link" /></svg></a>';
-    }
+    list[i].innerHTML +=
+      '&nbsp;<a class="deeplink" href="#' +
+      list[i].id +
+      '"><svg><use xlink:href="#symbol-link" /></svg></a>';
   }
 })(this);
 
