9839594227fe6c492052bbacd6aa14f65e2bd150
Nicolas Hoizey <nicolas@hoizey.com>
2019-03-31 23:02:57 +0200

    Prettier

--- _assets/javascript/analytics.js
+++ _assets/javascript/analytics.js
@@ -1,6 +1,6 @@
 // Hacked isogram with http://isogrammer.com/
 (function(n, h, o, i, z, e, y) {
-  n['GoogleAnalyticsObject'] = z;
+  n["GoogleAnalyticsObject"] = z;
   (n[z] =
     n[z] ||
     function() {
@@ -11,22 +11,22 @@
   e.async = 1;
   e.src = i;
   y.parentNode.insertBefore(e, y);
-})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+})(window, document, "script", "//www.google-analytics.com/analytics.js", "ga");
 
-ga('create', 'UA-1655999-10', 'auto');
+ga("create", "UA-1655999-10", "auto");
 
 if (viewport_width > 0) {
-  ga('set', {
+  ga("set", {
     dimension1: screen_density,
     dimension2: viewport_width,
-    dimension3: root_font_size,
+    dimension3: root_font_size
   });
 } else {
-  ga('set', {
+  ga("set", {
     dimension1: screen_density,
     dimension2: 0,
-    dimension3: root_font_size,
+    dimension3: root_font_size
   });
 }
 
-ga('send', 'pageview');
+ga("send", "pageview");
