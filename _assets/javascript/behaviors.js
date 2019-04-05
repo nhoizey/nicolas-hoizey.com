42277df9e7d52601997872516c206631eb2407f0
Nicolas Hoizey <nicolas@hoizey.com>
2019-04-05 18:15:21 +0200

    Enhance search results

--- _assets/javascript/behaviors.js
+++ _assets/javascript/behaviors.js
@@ -3,9 +3,9 @@
  * ****************************************************************/
 
 (function(win, doc) {
-  doc.body.setAttribute('data-viewportwidth', win.viewport_width);
-  doc.body.setAttribute('data-screendensity', win.screen_density);
-  doc.body.setAttribute('data-rootfontsize', win.root_font_size);
+  doc.body.setAttribute("data-viewportwidth", win.viewport_width);
+  doc.body.setAttribute("data-screendensity", win.screen_density);
+  doc.body.setAttribute("data-rootfontsize", win.root_font_size);
 })(window, document);
 
 /*****************************************************************
@@ -16,7 +16,7 @@
 (function(w) {
   var i,
     list = w.document.querySelectorAll(
-      'article.main h2[id], article.main h3[id], article.main h4[id], article.main h5[id], article.main h6[id]',
+      "article.main h2[id], article.main h3[id], article.main h4[id], article.main h5[id], article.main h6[id]"
     ),
     nb = list.length;
 
@@ -29,13 +29,13 @@
 })(this);
 
 // Lazyload Webmentions avatars
-window.addEventListener('load', () => {
-  [].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
-    img.setAttribute('srcset', img.getAttribute('data-srcset'));
-    img.setAttribute('src', img.getAttribute('data-src'));
+window.addEventListener("load", () => {
+  [].forEach.call(document.querySelectorAll("img[data-src]"), function(img) {
+    img.setAttribute("srcset", img.getAttribute("data-srcset"));
+    img.setAttribute("src", img.getAttribute("data-src"));
     img.onload = function() {
-      img.removeAttribute('data-srcset');
-      img.removeAttribute('data-src');
+      img.removeAttribute("data-srcset");
+      img.removeAttribute("data-src");
     };
   });
 });
@@ -45,44 +45,12 @@ window.addEventListener('load', () => {
  * ****************************************************************/
 
 // Install Service Worker
-if ('serviceWorker' in navigator) {
+if ("serviceWorker" in navigator) {
   // https://slides.com/webmax/serviceworker-thebest/#/23
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js', { scope: '/' });
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js", { scope: "/" });
   });
 }
-
-// https://stackoverflow.com/a/18650828/717195
-// function formatBytes(a, b) {
-//   if (0 == a) return '0 Bytes';
-//   var c = 1024,
-//     d = b || 2,
-//     e = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
-//     f = Math.floor(Math.log(a) / Math.log(c));
-//   return parseFloat((a / Math.pow(c, f)).toFixed(d)) + ' ' + e[f];
-// }
-
-// Log storage quota usage
-// https://slides.com/webmax/serviceworker-thebest/#/35
-// if ('storage' in navigator && 'estimate' in navigator.storage) {
-//   navigator.storage.estimate().then(({ usage, quota }) => {
-//     console.log(
-//       'Using ' +
-//         formatBytes(usage) +
-//         ' out of ' +
-//         formatBytes(quota) +
-//         ' bytes.',
-//     );
-//   });
-// }
-
-// Clean Service Worker cache
-// https://adactio.com/journal/9888
-// window.addEventListener('load', function() {
-//   if (navigator.serviceWorker.controller) {
-//     navigator.serviceWorker.controller.postMessage({ command: 'trimCaches' });
-//   }
-// });
 
 /*****************************************************************
  * Deal with offline/online events
@@ -92,62 +60,62 @@ if ('serviceWorker' in navigator) {
 // https://www.youtube.com/watch?v=7fnpsF9tMXc
 
 let isOffline = false;
-window.addEventListener('load', checkConnectivity);
+window.addEventListener("load", checkConnectivity);
 
 // when the page has finished loading,
 // listen for future changes in connection
 function checkConnectivity() {
   updateConnectivityStatus();
-  window.addEventListener('online', updateConnectivityStatus);
-  window.addEventListener('offline', updateConnectivityStatus);
+  window.addEventListener("online", updateConnectivityStatus);
+  window.addEventListener("offline", updateConnectivityStatus);
 }
 
 // check if we're online, set a class on <body> if offline
 function updateConnectivityStatus() {
   let offlineNotificationToShow = false;
-  let offlineNotificationIcon = '';
-  let offlineNotificationType = '';
-  let offlineNotificationMessage = '';
+  let offlineNotificationIcon = "";
+  let offlineNotificationType = "";
+  let offlineNotificationMessage = "";
   let offlineNotificationElement = window.document.getElementById(
-    'offline-notification',
+    "offline-notification"
   );
 
-  if (typeof navigator.onLine !== 'undefined') {
+  if (typeof navigator.onLine !== "undefined") {
     if (!navigator.onLine) {
       // add 'offline' class to the body, for any CSS adjustment
-      document.body.classList.add('offline');
+      document.body.classList.add("offline");
 
       offlineNotificationToShow = true;
-      offlineNotificationIcon = 'offline';
-      if ('serviceWorker' in navigator) {
+      offlineNotificationIcon = "offline";
+      if ("serviceWorker" in navigator) {
         // If the browser supports Service Workers and the Cache API,
         // getting offline should be less stressful. Use a "warning"
         // message instead of an "error and provide a link to content
         // available in cache.
-        offlineNotificationType = 'warning';
+        offlineNotificationType = "warning";
         offlineNotificationMessage =
           'Désolé, <strong>vous ne semblez plus être connecté</strong>. Vous pouvez continuer à lire cette page, ou <a href="/offline.html">voir ce qui est dans votre cache</a>.';
       } else {
-        offlineNotificationType = 'error';
+        offlineNotificationType = "error";
         offlineNotificationMessage =
-          'Désolé, <strong>vous ne semblez plus être connecté</strong>. Vous pouvez continuer à lire cette page en attendant le retour de la connexion.';
+          "Désolé, <strong>vous ne semblez plus être connecté</strong>. Vous pouvez continuer à lire cette page en attendant le retour de la connexion.";
       }
     } else {
       // remove 'offline' class from the body
-      document.body.classList.remove('offline');
+      document.body.classList.remove("offline");
 
-      offlineNotificationIcon = 'online';
+      offlineNotificationIcon = "online";
       if (offlineNotificationElement) {
         offlineNotificationToShow = true;
-        offlineNotificationType = 'success';
+        offlineNotificationType = "success";
         offlineNotificationMessage =
-          '<strong>Vous être de nouveau connecté</strong> ! Vous pouvez reprendre une navigation normale sur le site.';
+          "<strong>Vous être de nouveau connecté</strong> ! Vous pouvez reprendre une navigation normale sur le site.";
       }
     }
 
     if (
       offlineNotificationToShow &&
-      !window.document.getElementById('offline-notification-static')
+      !window.document.getElementById("offline-notification-static")
     ) {
       // https://stackoverflow.com/a/25214113/717195
       let newOfflineNotificationElement = document.createRange()
@@ -161,13 +129,13 @@ function updateConnectivityStatus() {
       if (offlineNotificationElement) {
         offlineNotificationElement.parentNode.replaceChild(
           offlineNotificationElement,
-          newOfflineNotificationElement,
+          newOfflineNotificationElement
         );
       } else {
-        let mainElement = document.querySelector('main');
+        let mainElement = document.querySelector("main");
         mainElement.parentNode.insertBefore(
           newOfflineNotificationElement,
-          mainElement,
+          mainElement
         );
       }
     }
@@ -181,12 +149,12 @@ function updateConnectivityStatus() {
 // Utility function to get the search query from the URL query string
 // http://stackoverflow.com/a/901144/717195
 function getParameterByName(name) {
-  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)'),
+  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
     results = regex.exec(location.search);
   return results === null
-    ? ''
-    : decodeURIComponent(results[1].replace(/\+/g, ' '));
+    ? ""
+    : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
 var algoliaLinked = false;
@@ -195,20 +163,20 @@ var algoliaClient;
 var algoliaIndex;
 
 function onAlgoliaAvailable(callback) {
-  if (typeof algoliasearch === 'function') {
+  if (typeof algoliasearch === "function") {
     algoliaLoaded = true;
     algoliaClient = algoliasearch(algoliaApplicationId, algoliaApiKey);
     algoliaIndex = algoliaClient.initIndex(algoliaIndexName);
     callback();
   } else {
     if (!algoliaLinked) {
-      var algoliaScript = window.document.createElement('script');
+      var algoliaScript = window.document.createElement("script");
       algoliaScript.setAttribute(
-        'src',
-        '/assets/javascript/vendors/algoliasearchLite-3.24.6.min.js',
+        "src",
+        "/assets/javascript/vendors/algoliasearchLite-3.32.0.min.js"
       );
       window.document
-        .getElementsByTagName('head')[0]
+        .getElementsByTagName("head")[0]
         .appendChild(algoliaScript);
       algoliaLinked = true;
     }
@@ -218,21 +186,21 @@ function onAlgoliaAvailable(callback) {
   }
 }
 
-var $intro = window.document.getElementById('intro');
-var $input = window.document.getElementById('search_input');
-var $results = window.document.getElementById('search_results');
+var $intro = window.document.getElementById("intro");
+var $input = window.document.getElementById("search_input");
+var $results = window.document.getElementById("search_results");
 var $currentUrl = window.location.toString();
-var $currentContent = window.document.querySelector('main');
-var $searchContent = window.document.querySelector('.search');
+var $currentContent = window.document.querySelector("main");
+var $searchContent = window.document.querySelector(".search");
 var searchSettings = {
   hitsPerPage: 50,
-  facets: '*',
-  attributesToHighlight: 'title,tags',
-  attributesToSnippet: 'content:20',
+  facets: "*",
+  attributesToHighlight: "title,tags",
+  attributesToSnippet: "content:20"
 };
 
 // A search query may come from the URL query string
-var queryString = getParameterByName('q');
+var queryString = getParameterByName("q");
 if (queryString.length > 0) {
   $input.value = queryString;
   onAlgoliaAvailable(function() {
@@ -241,21 +209,21 @@ if (queryString.length > 0) {
 }
 
 // A search query may come from the user typing in the search field
-$input.addEventListener('keyup', function() {
+$input.addEventListener("keyup", function() {
   if ($input.value.length > 0) {
-    history.pushState(null, null, '/search.html?q=' + $input.value);
-    if ($intro) $intro.style.display = 'none';
-    $currentContent.style.display = 'none';
-    $searchContent.style.display = 'block';
+    history.pushState(null, null, "/search.html?q=" + $input.value);
+    if ($intro) $intro.style.display = "none";
+    $currentContent.style.display = "none";
+    $searchContent.style.display = "block";
     onAlgoliaAvailable(function() {
       algoliaIndex.search($input.value, searchSettings, searchCallback);
     });
   } else {
     history.pushState(null, null, $currentUrl);
-    if ($intro) $intro.style.display = 'block';
-    $currentContent.style.display = 'block';
-    $searchContent.style.display = 'none';
-    $results.innerHTML = '';
+    if ($intro) $intro.style.display = "block";
+    $currentContent.style.display = "block";
+    $searchContent.style.display = "none";
+    $results.innerHTML = "";
   }
 });
 
@@ -266,21 +234,7 @@ function searchCallback(err, content) {
     return;
   }
 
-  var months = [
-    'janvier',
-    'février',
-    'mars',
-    'avril',
-    'mai',
-    'juin',
-    'juillet',
-    'août',
-    'septembre',
-    'octobre',
-    'novembre',
-    'décembre',
-  ];
-  $results.innerHTML = '';
+  $results.innerHTML = "";
 
   if (err) {
     console.error(err);
@@ -290,103 +244,65 @@ function searchCallback(err, content) {
   var resultsNumber = content.hits.length;
 
   if (resultsNumber === 0) {
-    $results.innerHTML = '<p>No result, please change your search.</p>';
+    $results.innerHTML = "<p>No result, please change your search.</p>";
     return;
   }
 
-  var result,
-    results,
-    hit,
-    hit_type,
-    hit_title,
-    hit_excerpt,
-    hit_date,
-    hit_tags;
+  var result, results, hit, hit_title, hit_excerpt, hit_date, hit_tags;
 
   results =
-    '<p class="nb">' +
-    resultsNumber +
-    ' résultat' +
-    (resultsNumber > 1 ? 's' : '') +
-    ' :';
+    `<h1>${resultsNumber} content` +
+    (resultsNumber > 1 ? "contents contain" : "content contains") +
+    ` <em>${$input.value}</em></h1><div class="list"><ul class="list__items">`;
 
   for (var i = 0; i < resultsNumber; i++) {
     hit = content.hits[i];
-    result = '';
+    result = "";
 
-    hit_type = '';
-    switch (hit.type) {
-      case 'post':
-      case 'page':
-        hit_type = hit.type;
-      case 'document':
-        if (hit.collection === 'links') {
-          hit_type = 'link';
-        }
-    }
-
-    hit_date = '';
+    hit_date = "";
     if (hit.date) {
       js_hit_date = new Date(hit.date * 1000);
-      date_options = { year: 'numeric', month: 'long', day: 'numeric' };
+      date_options = { year: "numeric", month: "long", day: "numeric" };
 
-      if (hit.lang === 'en') {
-        hit_date = js_hit_date.toLocaleDateString('en-US', date_options);
+      if (hit.lang === "en") {
+        hit_date = js_hit_date.toLocaleDateString("en-US", date_options);
       } else {
-        hit_date = js_hit_date.toLocaleDateString('fr-FR', date_options);
+        hit_date = js_hit_date.toLocaleDateString("fr-FR", date_options);
       }
     }
 
-    // if (hit_type === 'link') {
-    //   hit_title = 'Link from ' + hit_date;
-    // } else {
     hit_title = hit._highlightResult.title.value;
-    // }
-
     hit_excerpt = hit._highlightResult.html
       ? hit._highlightResult.html.value
       : hit._snippetResult.content
       ? hit._snippetResult.content.value
       : hit.excerpt_html;
 
-    hit_tags = '';
+    hit_tags = "";
     if (hit._highlightResult.tags) {
       // Build the tags list
-      hit_tags = '';
+      hit_tags = "";
       hit_tags_number = hit._highlightResult.tags.length;
       for (var j = 0; j < hit_tags_number; j++) {
-        hit_tags = hit_tags + ', ' + hit._highlightResult.tags[j].value;
+        hit_tags = hit_tags + ", " + hit._highlightResult.tags[j].value;
       }
-      hit_tags = hit_tags.replace(/^, /, '');
+      hit_tags = hit_tags.replace(/^, /, "");
     }
 
-    result =
-      '<article class="' +
-      hit_type +
-      '"><a href="' +
-      hit.url +
-      '"><h2>' +
-      hit_title +
-      '</h2>' +
-      hit_excerpt +
-      '</a>';
+    result = `<li class="card list__item"><h2 class="card__title"><a href="${
+      hit.url
+    }">${hit_title}</a></h2><p class="card__excerpt">${hit_excerpt}</p>`;
     if (hit_date || hit_tags) {
-      result += '<footer><ul>';
+      result += '<footer><ul class="card__meta">';
       if (hit_date) {
-        result +=
-          '<li class="date"><svg class="icon"><use xlink:href="#symbol-date" /></svg> ' +
-          hit_date +
-          '</li>';
+        result += `<li class="date card__meta__item card__date"><svg class="icon"><use xlink:href="#symbol-date" /></svg> ${hit_date}</li>`;
       }
       if (hit_tags) {
-        result +=
-          '<li class="tags"><svg class="icon"><use xlink:href="#symbol-tags" /></svg> ' +
-          hit_tags +
-          '</li>';
+        result += `<li class="tags card__meta__item card__tags"><svg class="icon"><use xlink:href="#symbol-tags" /></svg> ${hit_tags}</li>`;
       }
-      result += '</ul></footer>';
+      result += "</ul></footer>";
     }
-    result += '</article>';
+    result += "</li>";
 
     results += result;
   }
