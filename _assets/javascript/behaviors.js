1893d113ee4d69575d162c47f41c89f5f2d531af
Nicolas Hoizey <nicolas@hoizey.com>
2019-04-08 10:26:39 +0200

    Use Chrome (and future standard) native lazyload on avatars

--- _assets/javascript/behaviors.js
+++ _assets/javascript/behaviors.js
@@ -28,18 +28,6 @@
   }
 })(this);
 
-// Lazyload Webmentions avatars
-window.addEventListener("load", () => {
-  [].forEach.call(document.querySelectorAll("img[data-src]"), function(img) {
-    img.setAttribute("srcset", img.getAttribute("data-srcset"));
-    img.setAttribute("src", img.getAttribute("data-src"));
-    img.onload = function() {
-      img.removeAttribute("data-srcset");
-      img.removeAttribute("data-src");
-    };
-  });
-});
-
 /*****************************************************************
  * PWA
  * ****************************************************************/
