9cf53c78d6e29d618ebde77cc3a9990be0e44086
Nicolas Hoizey <nicolas@hoizey.com>
2019-11-12 10:18:48 +0100

    Base configuration for Eleventy

---
+++ .eleventy.js
@@ -0,0 +1,26 @@
+module.exports = function (eleventyConfig) {
+
+  eleventyConfig
+    .addPassthroughCopy("src/assets")
+    .addPassthroughCopy("src/.well-known")
+    .addPassthroughCopy("src/.htaccess")
+    .addPassthroughCopy("src/manifest.webmanifest");
+
+  return {
+    templateFormats: [
+      "md",
+      "njk",
+    ],
+
+    markdownTemplateEngine: "liquid",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    passthroughFileCopy: true,
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "dist"
+    }
+  };
+};
