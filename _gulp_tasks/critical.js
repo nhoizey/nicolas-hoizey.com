8544c4cb1fd879e0457d23d426bd31ffddec6ba6
Nicolas Hoizey <nicolas@hoizey.com>
2019-11-13 00:27:16 +0100

    Prettier

--- _gulp_tasks/critical.js
+++ _gulp_tasks/critical.js
@@ -3,20 +3,20 @@ const gulp = require('gulp')
 const critical = require('critical').stream
 
 const criticalConfig = {
-    inline: true,
-    base: config.buildDest + '/',
-    minify: true,
-    width: 1280,
-    height: 800,
-    ignore: ['@font-face']
+  inline: true,
+  base: config.buildDest + '/',
+  minify: true,
+  width: 1280,
+  height: 800,
+  ignore: ['@font-face']
 }
 
-gulp.task('critical', function() {
-    return gulp
-        .src(config.buildDest + '/index.html')
-        .pipe(critical(criticalConfig))
-        .on('error', function(err) {
-            console.error(err.message)
-        })
-        .pipe(gulp.dest(config.buildDest))
+gulp.task('critical', function () {
+  return gulp
+    .src(config.buildDest + '/index.html')
+    .pipe(critical(criticalConfig))
+    .on('error', function (err) {
+      console.error(err.message)
+    })
+    .pipe(gulp.dest(config.buildDest))
 })
