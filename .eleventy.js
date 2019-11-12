86d862e31597a45641cf6b20c4d3fb554b39ca86
Nicolas Hoizey <nicolas@hoizey.com>
2019-11-12 11:54:36 +0100

    Add Markdown-it plugins

--- .eleventy.js
+++ .eleventy.js
@@ -10,8 +10,45 @@ module.exports = function (eleventyConfig) {
   const permalinkDate = require("./src/_filters/permalinkDate.js");
   eleventyConfig.addFilter("permalinkDate", permalinkDate);
 
+  // ------------------------------------------------------------------------
+  // Markdown
+  // ------------------------------------------------------------------------
+
+  const markdownIt = require("markdown-it");
+  const markdownItOptions = {
+    html: true,
+    breaks: true,
+    linkify: true
+  };
+
   const markdownItFootnote = require("markdown-it-footnote");
+
+  const markdownItAnchor = require("markdown-it-anchor");
+  const slugify = require("@sindresorhus/slugify");
+  const markdownItAnchorOptions = {
+    permalink: true,
+    permalinkClass: "direct-link",
+    permalinkSymbol: "#",
+    level: [2, 3, 4],
+    slugify: function (s) {
+      return slugify(s);
+    }
+  };
+
+  const markdownItContainer = require("markdown-it-container");
+
+  eleventyConfig.setLibrary(
+    "md",
+    markdownIt(markdownItOptions)
       .use(markdownItFootnote)
+      .use(markdownItAnchor, markdownItAnchorOptions)
+      .use(markdownItContainer, "info")
+  );
+
+  // ------------------------------------------------------------------------
+  // Eleventy configuration
+  // ------------------------------------------------------------------------
+
   eleventyConfig
     .addPassthroughCopy("src/assets")
     .addPassthroughCopy("src/.well-known")
