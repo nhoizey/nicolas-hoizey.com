698571a7078a1cb90d0d80b0d81282c27851d45f
Nicolas Hoizey <nicolas@hoizey.com>
2019-11-16 22:49:08 +0100

    Copy images to the right location

--- _gulp_tasks/copy.js
+++ _gulp_tasks/copy.js
@@ -1,13 +1,14 @@
 const config = require('./_config.json')
 const gulp = require('gulp')
+const merge = require('merge-stream');
 
 gulp.task('copy', function () {
-  const linksDir = config.buildSrc + '/links'
-  const linksMediaGlobs = [
-    linksDir + '/**/*.{jpg,jpeg,png,gif,webp,mp3,mp4,webm,ogg,pdf,zip}'
-  ]
+  const mediaGlobs = '/**/*.{jpg,jpeg,png,gif,webp,mp3,mp4,webm,ogg,pdf,zip}';
+  const srcArticles = config.buildSrc + '/articles' + mediaGlobs;
+  const srcLinks = config.buildSrc + '/links' + mediaGlobs;
 
-  return gulp
-    .src(linksMediaGlobs, { base: linksDir })
-    .pipe(gulp.dest(config.assetDest + '/media'))
+  const articles = gulp.src(srcArticles).pipe(gulp.dest(config.assetDest + '/articles'));
+  const links = gulp.src(srcLinks).pipe(gulp.dest(config.assetDest + '/links'));
+
+  return merge(articles, links);
 })
