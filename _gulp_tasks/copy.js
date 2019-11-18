3f54925c292d96114ff660e1a94b588f711607a3
Nicolas Hoizey <nicolas@hoizey.com>
2019-11-18 10:01:01 +0100

    Use a single rename flow to copy content assets

--- _gulp_tasks/copy.js
+++ _gulp_tasks/copy.js
@@ -1,14 +1,16 @@
 const config = require('./_config.json')
 const gulp = require('gulp')
-const merge = require('merge-stream');
+const rename = require('gulp-rename');
 
 gulp.task('copy', function () {
-  const mediaGlobs = '/**/*.{jpg,jpeg,png,gif,webp,mp3,mp4,webm,ogg,pdf,zip}';
-  const srcArticles = config.buildSrc + '/articles' + mediaGlobs;
-  const srcLinks = config.buildSrc + '/links' + mediaGlobs;
 
-  const articles = gulp.src(srcArticles).pipe(gulp.dest(config.assetDest + '/articles'));
-  const links = gulp.src(srcLinks).pipe(gulp.dest(config.assetDest + '/links'));
-
-  return merge(articles, links);
+  const mediaGlobs = config.buildSrc + '/**/*.{jpg,jpeg,png,gif,webp,mp3,mp4,webm,ogg,pdf,zip}';
+  return gulp
+    .src(mediaGlobs)
+    .pipe(rename(function (path) {
+      if (path.dirname.match(/^(articles|links)\//)) {
+        path.dirname = path.dirname.replace(/\/([0-9]{2})\/([^/]+)$/, "/$2/");
+      }
+    }))
+    .pipe(gulp.dest(config.assetDest));
 })
