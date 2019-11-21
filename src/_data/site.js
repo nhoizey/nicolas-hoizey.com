ae629458d85acb6dd26f8750c3eb17e00252c999
Nicolas Hoizey <nicolas@hoizey.com>
2019-11-21 11:54:39 +0100

    Don't polute the package.json with site informations

---
+++ src/_data/site.js
@@ -0,0 +1,17 @@
+module.exports = {
+  name: "Nicolas Hoizey",
+  url: "https://eleventy.nicolas-hoizey.com",
+  title: "Nicolas Hoizey, passionate about the Web and photography",
+  description: "This is the personnal website of Nicolas Hoizey, passionate about the Web and photography, among many other interests.",
+  author: {
+    name: "Nicolas Hoizey",
+    email: "nicolas@hoizey.com",
+    url: "https://nicolas-hoizey.com/",
+    twitter: "nhoizey",
+    github: "nhoizey"
+  },
+
+  // Critical CSS results in much slower build times and uses a lot of system resources
+  // See `site/transforms/critical-css-transform.js` for more details
+  criticalCSS: false
+};
