d07822e56d3a8cb07082096ec1cf4e9f18f36217
Nicolas Hoizey <nicolas@hoizey.com>
2019-11-26 23:35:25 +0100

    Clean useless Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,8 @@ require('require-dir')('./_gulp_tasks')
 // Run the asset pipeline
 gulp.task('assets', gulp.parallel('icons', 'copy'))
 
-// Development Build Process
-gulp.task('build:dev', gulp.series('clean', 'generate', 'assets'))
-
 // Production Build Process
 gulp.task(
   'build',
-  gulp.series('clean', 'generate', 'assets', 'critical', 'workbox')
+  gulp.series('clean', 'assets', 'workbox')
 )
