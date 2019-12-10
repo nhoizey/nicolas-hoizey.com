72c017a4d293701e2f1d3365a10b48a519f1136a
Nicolas Hoizey <nicolas@hoizey.com>
2019-12-11 00:27:22 +0100

    Clean files

--- src/_filters/excerpt.js
+++ src/_filters/excerpt.js
@@ -5,7 +5,6 @@ module.exports = function excerpt(content) {
   const regex = /(<p( [^>]*)?>((?!(<\/p>)).|\n)+<\/p>)/m;
   let excerpt = '';
   cleanContent = content
-    // .replace(/<blockquote>(?!(<\/blockquote>))*<\/blockquote>/, '')
     .replace(/<p><img [^>]+><\/p>/, '');
   if ((matches = regex.exec(cleanContent)) !== null) {
     excerpt = matches[0];
