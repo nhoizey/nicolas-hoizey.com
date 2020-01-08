225f4de5855278be7edb19dfd38572e920d544e9
Nicolas Hoizey <nicolas@hoizey.com>
2020-01-08 12:27:08 +0100

    Useless old Jekyll code

--- _plugins/frequent_tags.rb
+++
@@ -1,9 +0,0 @@
-module FrequentTagsFilter
-  def frequent_tags(input)
-    # Remove tags with level 1
-    # <a href="/tags/api.html" class="set-1">API</a>
-    input.gsub(/<a href="[^"]+" class="set-1">[^<]+<\/a>/, '')
-  end
-end
-
-Liquid::Template.register_filter(FrequentTagsFilter)
