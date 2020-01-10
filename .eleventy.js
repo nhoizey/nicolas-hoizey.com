5aa99eeee4bd5e9d2c759ce3297081832a7e4018
Nicolas Hoizey <nicolas@hoizey.com>
2020-01-10 14:43:15 +0100

    Fix opengraph images

--- .eleventy.js
+++ .eleventy.js
@@ -134,6 +134,22 @@ module.exports = function (eleventyConfig) {
     return `<div class="giphy">${videoTag}</div>`;
   });
 
+  const getShareImage = require('@jlengstorf/get-share-image').default;
+  eleventyConfig.addNunjucksShortcode('ogImage', (title) => {
+    return title ? getShareImage({
+      imageWidth: 1200,
+      imageHeight: 630,
+      cloudName: 'nho',
+      imagePublicID: 'resources/opengraph-background',
+      titleFont: 'Noto Serif',
+      textAreaWidth: 1100,
+      textLeftOffset: 50,
+      titleBottomOffset: 330,
+      titleFontSize: 50 + Math.max(0, 30 - title.length),
+      title: title
+    }) : '';
+  });
+
   eleventyConfig.addPairedShortcode("markdown", (content, inline = null) => {
     return inline
       ? markdownIt.renderInline(content)
