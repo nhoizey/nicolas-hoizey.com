23c4785543016d253b8ec6de17d724905bd25cc8
Nicolas Hoizey <nicolas@hoizey.com>
2020-01-17 13:33:21 +0100

    Remove paragraph tag from excerpt

--- src/_filters/excerpt.js
+++ src/_filters/excerpt.js
@@ -4,10 +4,14 @@ module.exports = function excerpt(content) {
   }
   const regex = /(<p( [^>]*)?>((?!(<\/p>)).|\n)+<\/p>)/m;
   let excerpt = '';
+
+  // Remove paragraphs containing only an image
   cleanContent = content
     .replace(/<p><img [^>]+><\/p>/, '');
+
+  // Get first paragraph, if there's at least one, and remove the paragraph tag
   if ((matches = regex.exec(cleanContent)) !== null) {
-    excerpt = matches[0];
+    excerpt = matches[0].replace(/<p( [^>]*)?>(((?!(<\/p>)).|\n)+)<\/p>/, "$2");
   }
 
   return excerpt;
