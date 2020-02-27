5bc53bbe9bf1ed105ee5fe48c84501499fc728af
Nicolas Hoizey <nicolas@hoizey.com>
2020-02-27 23:12:07 +0100

    merge hashtag and tags

--- src/notes/notes.11tydata.js
+++ src/notes/notes.11tydata.js
@@ -9,8 +9,12 @@ module.exports = {
         return data.tags || [];
       }
 
-      let twitterHashtags = twitter.extractHashtags(twitter.htmlEscape(data.content));
-      return twitterHashtags || data.tags || [];
+      let tags = twitter.extractHashtags(twitter.htmlEscape(data.content));
+      if (data.tags !== undefined) {
+        // merge and deduplicate
+        tags = [...new Set([].concat(...tags, ...data.tags))]
+      }
+      return tags;
     }
   }
 };
