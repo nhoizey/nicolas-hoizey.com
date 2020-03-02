7616ef347af17584e3491c85c10f7606d10b7a10
Nicolas Hoizey <nicolas@hoizey.com>
2020-03-02 12:33:30 +0100

    Be more specific about resources to precache

--- workbox-config.js
+++ workbox-config.js
@@ -5,10 +5,11 @@ const BUILD_DIR = 'dist';
 module.exports = {
   globDirectory: BUILD_DIR,
   globPatterns: [
-    '**/*.{js,css}',
-    'index.html',
-    'offline.html',
-    'offline-fallback.html'
+    './index.html',
+    './js/*.js',
+    './css/*.css',
+    './offline.html',
+    './offline-fallback.html'
   ],
   swSrc: path.join(BUILD_DIR, 'sw.js'),
   swDest: path.join(BUILD_DIR, 'sw.js'),
