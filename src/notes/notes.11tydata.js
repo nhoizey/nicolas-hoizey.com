c31ef2846d9311f80a3da737c9c1a5a9dd3285ff
Nicolas Hoizey <nicolas@hoizey.com>
2020-04-06 16:00:43 +0200

    Prettier

--- src/notes/notes.11tydata.js
+++ src/notes/notes.11tydata.js
@@ -1,11 +1,11 @@
 const twitter = require('twitter-text');
 
 module.exports = {
-  layout: "note",
-  lang: "en",
-  permalink: "/notes/{{ page.date | notePermalinkDate }}/{{ page.fileSlug }}/",
+  layout: 'note',
+  lang: 'en',
+  permalink: '/notes/{{ page.date | notePermalinkDate }}/{{ page.fileSlug }}/',
   eleventyComputed: {
-    tags: data => {
+    tags: (data) => {
       if (data.content === undefined) {
         return data.tags || [];
       }
@@ -13,9 +13,9 @@ module.exports = {
       let tags = twitter.extractHashtags(twitter.htmlEscape(data.content));
       if (data.tags !== undefined) {
         // merge and deduplicate
-        tags = [...new Set([].concat(...tags, ...data.tags))]
+        tags = [...new Set([].concat(...tags, ...data.tags))];
       }
       return tags;
-    }
-  }
+    },
+  },
 };
