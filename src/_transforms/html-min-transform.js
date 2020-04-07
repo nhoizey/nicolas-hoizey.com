81a45d1c8502f2dcace9cc60553b9ee3d6606bc5
Nicolas Hoizey <nicolas@hoizey.com>
2020-04-07 15:49:34 +0200

    CSS is already minified

--- src/_transforms/html-min-transform.js
+++ src/_transforms/html-min-transform.js
@@ -9,7 +9,7 @@ module.exports = function htmlMinTransform(value, outputPath) {
     useShortDoctype: true,
     removeComments: true,
     collapseWhitespace: true,
-    minifyCSS: true,
+    minifyCSS: false,
   });
   return minified;
 };
