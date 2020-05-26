d9f67a00d82d85175676f034fdc9b50cd9040e02
Nicolas Hoizey <nicolas@hoizey.com>
2020-05-26 12:46:24 +0200

    No need to copy the raw Markdown for this

--- src/_11ty/collections/notes.js
+++ src/_11ty/collections/notes.js
@@ -4,12 +4,15 @@ const getFilteredCollection = require('../../_utils/filter-collection');
 module.exports = {
   notes: (collection) => {
     return getFilteredCollection(collection, 'notes').map((note) => {
-      let raw = note.template.frontMatter.content;
       // TODO: deal with hashtags only once
       note.data.tags = [
-        ...new Set([].concat(...note.data.tags, ...hashtagsToTags(raw))),
+        ...new Set(
+          [].concat(
+            ...note.data.tags,
+            ...hashtagsToTags(note.template.frontMatter.content)
+          )
+        ),
       ];
-      note.data.rawContent = raw;
       return note;
     });
   },
