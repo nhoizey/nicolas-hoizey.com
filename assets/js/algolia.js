757e69818bb209b1366f428dddbf04ccf460dd91
Nicolas Hoizey <nicolas@hoizey.com>
2020-07-28 19:43:03 +0200

    Use Algolia's npm packages

---
+++ assets/js/algolia.js
@@ -0,0 +1,54 @@
+// Based on
+// https://github.com/creasoft-dev/fundamenty/blob/master/src/scripts/algolia.js
+
+/*
+ * Algolia's autocomplete using instantsearch
+ * Requires following env variables:
+ * ALGOLIA_APP_ID
+ * ALGOLIA_SEARCH_API_KEY
+ * ALGOLIA_INDEX_NAME
+ */
+import algoliasearch from 'algoliasearch/lite';
+import autocomplete from 'autocomplete.js';
+
+const searchClient = algoliasearch(
+  process.env.ALGOLIA_APPLICATION_ID,
+  process.env.ALGOLIA_READ_ONLY_API_KEY
+);
+var index = searchClient.initIndex(process.env.ALGOLIA_INDEX_NAME);
+
+// Adapter for the new version 4 of algoliasearch js library
+function newHitsSource(index, params) {
+  return function doSearch(query, cb) {
+    index
+      .search(query, params)
+      .then(function (res) {
+        cb(res.hits, res);
+      })
+      .catch(function (err) {
+        console.error(err);
+        cb([]);
+      });
+  };
+}
+
+autocomplete('#search_input', { hint: false }, [
+  {
+    source: newHitsSource(index, { hitsPerPage: 5 }),
+    displayKey: 'content',
+    templates: {
+      suggestion: function (suggestion) {
+        return (
+          suggestion._highlightResult.title.value +
+          ' (' +
+          suggestion._highlightResult.tags.value +
+          ')'
+        );
+      },
+    },
+  },
+]).on('autocomplete:selected', function (event, suggestion, dataset) {
+  console.log(suggestion, dataset);
+  // alert('dataset: ' + dataset + ':  ' + suggestion, null, 2);
+  window.location.replace(suggestion.url);
+});
