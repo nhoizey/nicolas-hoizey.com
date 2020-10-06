8f08570fdd32acc57d86905fbaec660b07b586c8
Nicolas Hoizey <nicolas@hoizey.com>
2020-10-06 16:46:14 +0200

    Use Feather icons and automate sprite generation
    Fixes #60

--- assets/svg-icons/README.md
+++ assets/svg-icons/README.md
@@ -1,8 +1,4 @@
 # Icons
 
-https://github.com/outpunk/evil-icons/tree/master/assets/icons
-
-http://designmodo.com/linecons-free
-
-https://thenounproject.com/term/wifi/628438/
-https://thenounproject.com/term/disconnected/625408/
+Feather icons
+https://feather.netlify.app/
--- src/_utils/generate-svg-sprite.js
+++ src/_utils/generate-svg-sprite.js
@@ -2,18 +2,39 @@ const svgstore = require('svgstore');
 const fs = require('fs');
 const path = require('path');
 
-const ICONS_FOLDER = 'assets/svg-icons/';
+const ICONS_FOLDER = 'assets/svg-icons/feather/';
+const ICONS_LIST = {
+  calendar: 'Date',
+  info: 'Info',
+  link: 'Link',
+  wifi: 'Online',
+  'wifi-off': 'Offline',
+  search: 'Search',
+  tag: 'Tag',
+  twitter: 'Twitter',
+  'message-circle': 'Webmention',
+};
 
 let sprite = svgstore({
   svgAttrs: { style: 'display: none;', 'aria-hidden': 'true' },
   copyAttrs: ['width', 'height'],
 });
-fs.readdirSync(ICONS_FOLDER).forEach((icon) => {
-  console.log(icon);
-  sprite.add(
-    `symbol-${path.basename(icon, '.svg')}`,
-    fs.readFileSync(path.join(ICONS_FOLDER, icon), 'utf8')
-  );
+
+Object.entries(ICONS_LIST).forEach(([icon, title]) => {
+  console.log(`${icon}.svg -> ${title}`);
+  const svgFile = fs
+    .readFileSync(path.join(ICONS_FOLDER, `${icon}.svg`), 'utf8')
+    .replace(' width="24" height="24"', ' width="1em" height="1em"')
+    .replace(
+      / fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-[^"]+">/,
+      ` ><title id="${icon}-icon">${title}</title>`
+    );
+  sprite.add(`symbol-${icon}`, svgFile, {
+    symbolAttrs: {
+      'aria-labelledby': `${icon}-icon`,
+      role: 'img',
+    },
+  });
 });
 fs.writeFileSync(
   'src/_includes/svg-sprite.njk',
