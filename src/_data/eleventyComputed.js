b9e5ca46453b2fb84c653d8f6db6a02a0ffea790
Nicolas Hoizey <nicolas@hoizey.com>
2020-10-17 00:49:30 +0200

    Add computed data to all content

---
+++ src/_data/eleventyComputed.js
@@ -0,0 +1,31 @@
+const dtf = {
+  en: new Intl.DateTimeFormat('en-GB', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }),
+  fr: new Intl.DateTimeFormat('fr-FR', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }),
+};
+
+const dtfDigits = new Intl.DateTimeFormat('en-GB', {
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+});
+
+const attributeDate = (date) =>
+  dtfDigits.format(date).split('/').reverse().join('-');
+
+const permalinkDate = (date) =>
+  dtfDigits.format(date).split('/').reverse().join('/');
+
+module.exports = {
+  lang: (data) => data.lang || 'en',
+  formattedDate: (data) => dtf[data.lang || 'en'].format(data.page.date),
+  attributeDate: (data) => attributeDate(data.page.date),
+  permalinkDate: (data) => permalinkDate(data.page.date),
+};
