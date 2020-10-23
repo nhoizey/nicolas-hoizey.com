422c2bf9d62d12ec12c4efa5809de00e63a1a84e
Nicolas Hoizey <nicolas@hoizey.com>
2020-10-23 09:56:41 +0200

    Organize collections

--- src/_11ty/collections/contents.js
+++
@@ -1,7 +0,0 @@
-const getFilteredCollection = require('../../_utils/filter-collection');
-
-module.exports = {
-  contents: (collection) => {
-    return getFilteredCollection(collection, '{articles,links,notes}');
-  },
-};
