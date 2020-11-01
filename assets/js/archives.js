f58b125bb60a8ebc05a8c6600625b51f30c99ad8
Nicolas Hoizey <nicolas@hoizey.com>
2020-11-01 01:15:28 +0100

    Improve archives with Algolia

--- assets/js/archives.js
+++ assets/js/archives.js
@@ -11,8 +11,12 @@ import {
   searchBox,
   stats,
 } from 'instantsearch.js/es/widgets';
+import { history } from 'instantsearch.js/es/lib/routers';
 
-const titleize = (string) => string.charAt(0).toUpperCase() + string.slice(1);
+const titleize = (string) => {
+  if (string === undefined) return '';
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
 
 // https://stackoverflow.com/a/46851765/717195
 const unescapeHtml = (input) => {
@@ -23,16 +27,27 @@ const unescapeHtml = (input) => {
   });
 };
 
+const activateNavItem = (type) => {
+  document.querySelectorAll('.navigation li').forEach((li) => {
+    const link = li.querySelectorAll(`a[href="/${type}/"]`);
+    // TODO: also update ARIA
+    if (link.length === 1) {
+      li.classList.add('current');
+    } else {
+      li.classList.remove('current');
+    }
+  });
+};
+
 const search = instantsearch({
   indexName: process.env.ALGOLIA_INDEX_NAME,
   searchClient: algoliasearch(
     process.env.ALGOLIA_APP_ID,
     process.env.ALGOLIA_READ_ONLY_API_KEY
   ),
-  routing: true,
-  initialUiState: {
-    nho: window.instantsearchInitialUiState || {},
-  },
+  // initialUiState: {
+  //   nho: window.instantsearchInitialUiState || {},
+  // },
   // searchFunction(helper) {
   // if (window.instantsearchInitialUiState) {
   //   window.instantsearchInitialUiState = false;
@@ -46,30 +61,148 @@ const search = instantsearch({
   // .setPage(page) // we re-apply the previous page
   // .search();
   // },
-  onStateChange({ uiState, setUiState }) {
-    let contentType = 'archives';
-    let typeTitle = 'Archives';
-    if (
-      uiState.nho.refinementList &&
-      uiState.nho.refinementList.type &&
-      uiState.nho.refinementList.type.length === 1
-    ) {
-      contentType = uiState.nho.refinementList.type[0];
-      typeTitle = titleize(contentType);
-    }
-    document.querySelector('h1').innerText = typeTitle;
-    document.title = `${typeTitle} - Nicolas Hoizey`;
-    document.querySelectorAll('.navigation li').forEach((li) => {
-      const link = li.querySelectorAll(`a[href="/${contentType}/"]`);
-      // TODO: also update ARIA
-      if (link.length === 1) {
-        li.classList.add('current');
-      } else {
-        li.classList.remove('current');
-      }
-    });
-    // TODO: update URL with https://www.algolia.com/doc/guides/building-search-ui/going-further/routing-urls/js/
-    setUiState(uiState);
+  routing: {
+    router: history({
+      windowTitle({ type, query }) {
+        let typeSlug = 'archives';
+        let title = '';
+        if (query) {
+          title += `Search for "${query}"`;
+          if (type !== undefined && type.length === 1) {
+            typeSlug = type[0];
+            title += ` in ${type[0]}`;
+          } else {
+            title += ' in archives';
+          }
+        } else {
+          if (type !== undefined && type.length === 1) {
+            typeSlug = type[0];
+            title += titleize(type[0]);
+          } else {
+            title += 'Archives';
+          }
+        }
+        window.document.querySelector('h1').innerText = title;
+        title += ' - Nicolas Hoizey';
+
+        activateNavItem(typeSlug);
+
+        return title;
+      },
+      createURL({ qsModule, routeState, location }) {
+        // console.log('createURL');
+        // console.dir(routeState);
+        const queryParameters = {};
+
+        let urlPath = 'archives';
+        if (routeState.type !== undefined) {
+          switch (routeState.type.length) {
+            case 0:
+              break;
+            case 1:
+              urlPath = routeState.type[0];
+              break;
+            default:
+              queryParameters.type = routeState.type;
+          }
+        }
+        if (routeState.date !== undefined) {
+          urlPath += `/${routeState.date.replace('-', '/')}`;
+        }
+        if (routeState.query) {
+          queryParameters.query = encodeURIComponent(routeState.query);
+        }
+        if (routeState.page && routeState.page !== 1) {
+          queryParameters.page = routeState.page;
+        }
+        if (routeState.tags && routeState.tags.length > 0) {
+          queryParameters.tags = routeState.tags;
+        }
+
+        const queryString = qsModule.stringify(queryParameters, {
+          addQueryPrefix: true,
+          arrayFormat: 'repeat',
+        });
+
+        return `/${urlPath}/${queryString}`;
+      },
+
+      parseURL({ qsModule, location }) {
+        console.log('parseURL');
+        console.log(location);
+        return {};
+        const pathnameMatches = location.pathname.match(/\/(.*?)\/?$/);
+        let type = (pathnameMatches && pathnameMatches[1]) || '';
+        const { query = '', page = 1 } = qsModule.parse(
+          location.search.slice(1)
+        );
+        // console.log({ type, query, page });
+        return {
+          query: decodeURIComponent(query),
+          page: page,
+          type: type,
+        };
+      },
+    }),
+    stateMapping: {
+      routeToState(routeState) {
+        // console.log('routeToState');
+        // console.dir(routeState);
+        const state = { nho: {} };
+        if (routeState.query !== undefined && routeState.query !== '') {
+          state.nho.query = routeState.query;
+        }
+        if (routeState.page !== undefined && routeState.page !== 1) {
+          state.nho.page = routeState.page;
+        }
+        ['type', 'lang', 'tags'].forEach((refinement) => {
+          if (
+            routeState[refinement] !== undefined &&
+            routeState[refinement].length > 0
+          ) {
+            if (state.nho.refinementList === undefined) {
+              state.nho.refinementList = {};
+            }
+            state.nho.refinementList[refinement] = routeState[refinement];
+          }
+        });
+        if (routeState.date !== undefined && routeState.date.length > 0) {
+          state.nho.hierarchicalMenu = {
+            'date.lvl0': routeState.date.split('-'),
+          };
+        }
+        return state;
+      },
+      stateToRoute(uiState) {
+        // console.log('stateToRoute');
+        // console.dir(uiState);
+        const route = {};
+        if (uiState['nho'].query !== undefined && uiState['nho'].query !== '') {
+          route.query = uiState['nho'].query;
+        }
+        if (uiState['nho'].page !== undefined && uiState['nho'].page !== 1) {
+          route.page = uiState['nho'].page;
+        }
+        if (uiState['nho'].refinementList !== undefined) {
+          ['type', 'lang', 'tags'].forEach((refinement) => {
+            if (
+              uiState.nho.refinementList[refinement] !== undefined &&
+              uiState.nho.refinementList[refinement].length !== 0
+            ) {
+              route[refinement] = uiState.nho.refinementList[refinement];
+            }
+          });
+        }
+        if (
+          uiState.nho.hierarchicalMenu !== undefined &&
+          uiState.nho.hierarchicalMenu['date.lvl0'] !== undefined &&
+          uiState.nho.hierarchicalMenu['date.lvl0'].length !== 0
+        ) {
+          route.date = uiState.nho.hierarchicalMenu['date.lvl0'].join('-');
+        }
+        return route;
+      },
+    },
   },
 });
 
