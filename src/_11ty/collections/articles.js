a8c698c65b5d3b3799e57d02fe81d7e618818b21
Nicolas Hoizey <nicolas@hoizey.com>
2020-11-19 00:35:14 +0100

    Compute latest and promoted articles collections

--- src/_11ty/collections/articles.js
+++ src/_11ty/collections/articles.js
@@ -1,19 +1,21 @@
 const getFilteredCollection = require('../../_utils/filter-collection');
 
+const latestNb = 3;
+const promotedNb = 3;
+
 module.exports = {
   articles: (collection) => {
     return getFilteredCollection(collection, 'articles');
   },
-  promoted: (collection) => {
-    // promoted articles, but not the latest article at all
-    let fullCollection = collection
-      .getFilteredByGlob('src/articles/**/*.md')
-      .sort((a, b) => {
-        return b.date - a.date;
-      })
-      .slice(1)
-      .filter((article) => article.data.promoted);
-
-    return fullCollection;
+  latestArticles: (collection) => {
+    // latest articles
+    return getFilteredCollection(collection, 'articles').slice(0, latestNb);
+  },
+  promotedArticles: (collection) => {
+    // promoted articles within not the latest ones
+    return getFilteredCollection(collection, 'articles')
+      .slice(latestNb)
+      .filter((article) => article.data.promoted)
+      .slice(0, promotedNb);
   },
 };
