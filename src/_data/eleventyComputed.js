ef98676b497703f87db50faab1e66b217b8590ad
Nicolas Hoizey <nicolas@hoizey.com>
2020-11-27 22:24:01 +0100

    Compute page title in eleventyComputed.js

--- src/_data/eleventyComputed.js
+++ src/_data/eleventyComputed.js
@@ -19,6 +19,40 @@ const dtfDigits = new Intl.DateTimeFormat('en-GB', {
 
 const attributeDate = (date) =>
   dtfDigits.format(date).split('/').reverse().join('-');
+function textAuthors(data) {
+  let text = '';
+  if (data.layout === 'link' && data.authors && data.authors.length > 0) {
+    let i = 0;
+    const nb = data.authors.length;
+    data.authors.forEach((author) => {
+      text += author.name;
+      i++;
+      if (i < nb - 1) {
+        text += ', ';
+      }
+      if (i === nb - 1) {
+        text += ' and ';
+      }
+    });
+  }
+  return text;
+}
+
+function title(data) {
+  switch (data.layout) {
+    case 'link':
+      return `${textAuthors(data)}:\n“${data.title}”`;
+    case 'note':
+      return `Note from ${formattedDate(data.lang, data.page.date)}`;
+  }
+  if (data.title && data.title !== '') {
+    return data.title;
+  } else {
+    // TODO: console.log(`No title for ${data.page.inputPath}`);
+    return '???';
+  }
+}
+
 
 const permalinkDate = (date) =>
   dtfDigits.format(date).split('/').reverse().join('/');
@@ -37,6 +71,18 @@ function tags(data) {
   return tags;
 }
 
+function headTitle(data) {
+  if (data.page.url === '/') {
+    return `<title itemprop="name">${data.pkg.title}</title>`;
+  }
+  // if (data.layout === 'note') {
+  //   return `<title>${lead(data).slice(0, 50)} - ${
+  //     data.pkg.author.name
+  //   }</title>`;
+  // }
+  return `<title>${title(data)} - ${data.pkg.author.name}</title>`;
+}
+
 
 // TODO: is it useful?
 module.exports = {
@@ -44,5 +90,8 @@ module.exports = {
   formattedDate: (data) => dtf[data.lang || 'en'].format(data.page.date),
   attributeDate: (data) => attributeDate(data.page.date),
   permalinkDate: (data) => permalinkDate(data.page.date),
+  head: {
+    title: (data) => headTitle(data),
+  },
   tags: (data) => tags(data),
 };
