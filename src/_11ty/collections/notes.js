711993091ba9a0b6126c534c8781f72023158896
Nicolas Hoizey <nicolas@hoizey.com>
2020-11-30 10:07:55 +0100

    Sort tags

--- src/_11ty/collections/notes.js
+++ src/_11ty/collections/notes.js
@@ -5,14 +5,17 @@ module.exports = {
   notes: (collection) => {
     return getFilteredCollection(collection, 'notes').map((note) => {
       // TODO: deal with hashtags only once
-      note.data.tags = [
-        ...new Set(
-          [].concat(
-            ...note.data.tags,
-            ...hashtagsToTags(note.template.frontMatter.content)
-          )
-        ),
-      ];
+      let tags = hashtagsToTags(note.template.frontMatter.content);
+      if (note.data.tags) {
+        tags = [...new Set([].concat(...note.data.tags, ...tags))];
+      }
+      tags.sort((a, b) => {
+        return a.localeCompare(b, 'en', { ignorePunctuation: true });
+      });
+      // if (note.url === '/notes/2020/11/18/3/') {
+      //   console.dir({ url: note.url, tags: tags });
+      // }
+      note.data.tags = tags;
       return note;
     });
   },
