cc7d110e11fb2e445f9c438454c8e47d34a711dc
Nicolas Hoizey <nicolas@hoizey.com>
2020-12-01 00:39:15 +0100

    Add simple icons for brands logos

--- src/_utils/generate-svg-sprite.js
+++ src/_utils/generate-svg-sprite.js
@@ -3,23 +3,31 @@ const fs = require('fs');
 const path = require('path');
 
 // Where are Feather icons available from the npm package?
-const ICONS_FOLDER = 'node_modules/feather-icons/dist/icons/';
+const ICONS_FOLDERS = {
+  feather: 'node_modules/feather-icons/dist/icons/',
+  simple: 'node_modules/simple-icons/icons/',
+};
 
 // Which icons do I need for the sprite?
 // icon filename + title for accessibility
 const ICONS_LIST = {
-  anchor: { title: 'Anchor' },
-  calendar: { name: 'date', title: 'Date' },
-  info: { title: 'Info' },
-  link: { title: 'Link' },
-  'map-pin': { name: 'location', title: 'Location' },
-  'message-circle': { name: 'comments', title: 'Webmention' },
-  rss: { name: 'feeds', title: 'Feeds' },
-  search: { title: 'Search' },
-  tag: { name: 'tags', title: 'Tag' },
-  twitter: { title: 'Twitter' },
-  wifi: { name: 'online', title: 'Online' },
-  'wifi-off': { name: 'offline', title: 'Offline' },
+  feather: {
+    anchor: { title: 'Anchor' },
+    calendar: { name: 'date', title: 'Date' },
+    info: { title: 'Info' },
+    link: { title: 'Link' },
+    'map-pin': { name: 'location', title: 'Location' },
+    'message-circle': { name: 'comments', title: 'Webmention' },
+    rss: { name: 'feeds', title: 'Feeds' },
+    search: { title: 'Search' },
+    tag: { name: 'tags', title: 'Tag' },
+    twitter: { title: 'Twitter' },
+    wifi: { name: 'online', title: 'Online' },
+    'wifi-off': { name: 'offline', title: 'Offline' },
+  },
+  simple: {
+    mastodon: { title: 'Mastodon' },
+  },
 };
 
 // Initiate the sprite with svgstore
@@ -31,28 +39,33 @@ let sprite = svgstore({
 });
 
 // Loop through each icon in the list
-Object.entries(ICONS_LIST).forEach(([icon, properties]) => {
-  // Log the name of the icon and its title to the console
-  console.log(`${icon}.svg -> ${properties.title}`);
-  const svgFile = fs
-    // Load the content of the icon SVG file
-    .readFileSync(path.join(ICONS_FOLDER, `${icon}.svg`), 'utf8')
-    // Make its dimensions relative to the surounding text
-    .replace(' width="24" height="24"', ' width="1em" height="1em"')
-    // Clean useless attributes
-    .replace(
-      / fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-[^"]+">/,
-      ' >'
-    );
-  // Add the new symbol to the sprite
-  sprite.add(`symbol-${properties.name || icon}`, svgFile, {
-    // Add attributes for accessibility
-    symbolAttrs: {
-      'aria-label': properties.title,
-      role: 'img',
-    },
+Object.entries(ICONS_LIST).forEach(([source, icons]) => {
+  Object.entries(icons).forEach(([icon, properties]) => {
+    // Log the name of the icon and its title to the console
+    console.log(`${icon}.svg -> ${properties.title}`);
+    const svgFile = fs
+      // Load the content of the icon SVG file
+      .readFileSync(path.join(ICONS_FOLDERS[source], `${icon}.svg`), 'utf8')
+      // Remove dimensions from Feather icons
+      .replace('width="24" height="24"', '')
+      // Clean useless Feather attributes
+      .replace(
+        / fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-[^"]+">/,
+        ' >'
+      );
+    // Add the new symbol to the sprite
+    sprite.add(`symbol-${properties.name || icon}`, svgFile, {
+      // Add attributes for accessibility
+      symbolAttrs: {
+        width: '1em',
+        height: '1em',
+        'aria-label': properties.title,
+        role: 'img',
+      },
+    });
   });
 });
+
 // Finally, store the sprite in a file Eleventy will be able to include
 fs.writeFileSync(
   'src/_includes/svg-sprite.svg',
