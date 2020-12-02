7f743d2af7d51926b58eece6e7fd82590be0d0d7
Nicolas Hoizey <nicolas@hoizey.com>
2020-12-02 22:38:51 +0100

    Add Eleventy as generator

--- src/_data/build.js
+++ src/_data/build.js
@@ -1,7 +1,10 @@
+const eleventyPkg = require('@11ty/eleventy/package.json');
+
 const timestamp = new Date();
 
 module.exports = {
   env: process.env.NODE_ENV,
   timestamp: timestamp,
   id: timestamp.valueOf(),
+  version: eleventyPkg.version,
 };
