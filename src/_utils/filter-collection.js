e72130a6a42a0db554859dd32b24d2cf2491e60d
Nicolas Hoizey <nicolas@hoizey.com>
2021-01-17 14:09:24 +0100

    Don't show future content

--- src/_utils/filter-collection.js
+++ src/_utils/filter-collection.js
@@ -1,4 +1,5 @@
 let filteredCollectionsMemoization = {};
+let now = new Date().getTime();
 const getFilteredCollection = (collection, type) => {
   if (type in filteredCollectionsMemoization) {
     return filteredCollectionsMemoization[type];
@@ -6,6 +7,10 @@ const getFilteredCollection = (collection, type) => {
     const pattern = type === 'archives' ? '{articles,links,notes,talks}' : type;
     let filteredCollection = collection
       .getFilteredByGlob(`src/${pattern}/**/*.md`)
+      .filter((item) => {
+        if (now < item.date.getTime()) console.log(`${item.date.getTime()}`);
+        return now >= item.date.getTime();
+      })
       .sort((a, b) => b.date - a.date);
     filteredCollectionsMemoization[type] = filteredCollection;
 
