a377553716c7298e586e0e9595477bbcd00f39b0
Nicolas Hoizey <nicolas@hoizey.com>
2021-01-18 12:52:07 +0100

    Remove logs

--- src/_utils/filter-collection.js
+++ src/_utils/filter-collection.js
@@ -7,10 +7,7 @@ const getFilteredCollection = (collection, type) => {
     const pattern = type === 'archives' ? '{articles,links,notes,talks}' : type;
     let filteredCollection = collection
       .getFilteredByGlob(`src/${pattern}/**/*.md`)
-      .filter((item) => {
-        if (now < item.date.getTime()) console.log(`${item.date.getTime()}`);
-        return now >= item.date.getTime();
-      })
+      .filter((item) => now >= item.date.getTime())
       .sort((a, b) => b.date - a.date);
     filteredCollectionsMemoization[type] = filteredCollection;
 
