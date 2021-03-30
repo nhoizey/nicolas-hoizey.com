91076ef45ce1853fab89c1641dcc99a050b4ff00
Nicolas Hoizey <nhoizey@clever-age.com>
2021-03-31 01:18:32 +0200

    Start working on removing Moment.js

--- src/_11ty/filters/dates.js
+++ src/_11ty/filters/dates.js
@@ -1,15 +1,40 @@
 const moment = require('moment');
 
+const dtf = {
+  en: new Intl.DateTimeFormat('en-GB', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }),
+  fr: new Intl.DateTimeFormat('fr-FR', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }),
+};
+
+const dtfDigits = new Intl.DateTimeFormat('en-GB', {
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+});
+
+function formattedDate(lang, date) {
+  return dtf[lang || 'en'].format(date);
+}
+
 module.exports = {
   date: (date, format) => {
     return moment(date).format(format);
   },
   formattedDate: (date) => {
-    return moment(date).format('Do MMMM YYYY');
+    // return moment(date).format('Do MMMM YYYY');
+    return formattedDate('en', date);
   },
   monthString: (month) => {
     // transforms "2020/02" into "February 2020"
     let fullDate = `${month.replace('/', '-')}-01T10:00:00.000Z`;
     return moment(fullDate).format('MMMM YYYY');
   },
+  attributeDate: (date) => date.substr(0, 10),
 };
