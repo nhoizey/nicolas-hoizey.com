01e1b3817074950557cf99eefb19c6023e81444b
Nicolas Hoizey <nhoizey@clever-age.com>
2021-03-31 02:58:19 +0200

    Remove Netlify specificities

--- src/_11ty/filters/file.js
+++ src/_11ty/filters/file.js
@@ -4,10 +4,4 @@ module.exports = {
   dirname: (filePath) => {
     return typeof filePath === 'string' ? path.dirname(filePath) : '';
   },
-  newUrlToOld: (url) => {
-    return url.replace(
-      /^\/articles(\/[0-9]{4}\/[0-9]{2})\/[0-9]{2}\/(.*)\/$/,
-      '$1/$2'
-    );
-  },
 };
