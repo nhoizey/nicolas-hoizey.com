5836df24c764dc9f2a05860272e984b6005f7daa
Nicolas Hoizey <nhoizey@clever-age.com>
2021-06-28 14:27:18 +0200

    Configure stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,1 +1,20 @@
-module.exports = { extends: 'stylelint-config-standard' };
+module.exports = {
+  ignoreFiles: ['assets/sass/highlight/*.scss'],
+  extends: [
+    'stylelint-config-sass-guidelines',
+    'stylelint-config-idiomatic-order',
+    'stylelint-config-prettier',
+  ],
+  plugins: ['stylelint-order'],
+  rules: {
+    'order/order': [
+      'custom-properties',
+      'dollar-variables',
+      'declarations',
+      'rules',
+      'at-rules',
+    ],
+    'order/properties-order': ['width', 'height'],
+    'order/properties-alphabetical-order': null,
+  },
+};
