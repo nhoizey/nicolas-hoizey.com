d7a7aa6e633fda2e7196a2444f3ed2fc7da9b549
Nicolas Hoizey <nhoizey@clever-age.com>
2022-02-07 01:09:50 +0100

    Remove Algolia and tweets

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -4,9 +4,3 @@ ssh nhoizey@ssh-nhoizey.alwaysdata.net /home/nhoizey/backup-nhocom.sh
 
 echo "Sync server from local"
 /usr/local/bin/rsync -arvu --delete-delay --iconv=UTF8-MAC,UTF-8 ./_site/ nhoizey@ssh-nhoizey.alwaysdata.net:/home/nhoizey/www/nicolas-hoizey.com/www/ 2>&1 | tee ./_logs/deploy.log
-
-echo "Update Algolia index"
-npm run algolia
-
-echo "Send tweets"
-npm run tweets
