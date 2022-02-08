2ee1043d9a02f10a0416df22b10db2d18192dc10
Nicolas Hoizey <nhoizey@clever-age.com>
2022-02-08 01:13:01 +0100

    Refactor dates

--- src/_utils/dates.js
+++ src/_utils/dates.js
@@ -1,53 +1,37 @@
 const { DateTime } = require('luxon');
 
-const formattedDate = (date, lang = 'en') =>
-  DateTime.fromJSDate(date, { setZone: true })
-    .setZone('Europe/Paris')
-    .setLocale(lang)
-    .toFormat('d LLLL y');
-
-const formattedDateTime = (date, lang = 'en') =>
-  DateTime.fromJSDate(date, { setZone: true })
-    .setZone('Europe/Paris')
-    .setLocale(lang)
-    .toFormat('d LLLL y, HH:mm');
-
-const formattedDateTimeFromIso = (date, lang = 'en') =>
+const iso2DT = (date, lang = 'en') =>
   DateTime.fromISO(date, { setZone: true })
     .setZone('Europe/Paris')
-    .setLocale(lang)
-    .toFormat('d LLLL y, HH:mm');
+    .setLocale(lang);
+
+const js2DT = (date, lang = 'en') =>
+  DateTime.fromJSDate(date, { setZone: true })
+    .setZone('Europe/Paris')
+    .setLocale(lang);
+
+const formattedDate = (date, lang = 'en') =>
+  js2DT(date, lang).toFormat('d LLLL y');
+
+const formattedDateTime = (date, lang = 'en') =>
+  js2DT(date, lang).toFormat('d LLLL y, HH:mm');
+
+const formattedDateTimeFromIso = (date, lang = 'en') =>
+  iso2DT(date, lang).toFormat('d LLLL y, HH:mm');
 
 const monthString = (month, lang = 'en') => {
   // transforms "2020/02" into "February 2020"
   let fullDate = `${month.replace('/', '-')}-01T10:00:00.000Z`;
-  return DateTime.fromISO(fullDate, { setZone: true })
-    .setZone('Europe/Paris')
-    .setLocale(lang)
-    .toFormat('LLLL y');
+  return iso2DT(fullDate, lang).toFormat('LLLL y');
 };
 
-const dayMonth = (date, lang = 'en') =>
-  DateTime.fromJSDate(date, { setZone: true })
-    .setZone('Europe/Paris')
-    .setLocale(lang)
-    .toFormat('LLLL y');
+const dayMonth = (date, lang = 'en') => js2DT(date, lang).toFormat('LLLL y');
 
-const isoDate = (date) =>
-  DateTime.fromJSDate(date, { setZone: true }).setZone('Europe/Paris').toISO();
+const isoDate = (date) => js2DT(date).toISO();
 
-const year = (date) =>
-  DateTime.fromJSDate(date, { setZone: true })
-    .setZone('Europe/Paris')
-    .toFormat('y');
-const month = (date) =>
-  DateTime.fromJSDate(date, { setZone: true })
-    .setZone('Europe/Paris')
-    .toFormat('LL');
-const day = (date) =>
-  DateTime.fromJSDate(date, { setZone: true })
-    .setZone('Europe/Paris')
-    .toFormat('dd');
+const year = (date) => js2DT(date).toFormat('y');
+const month = (date) => js2DT(date).toFormat('LL');
+const day = (date) => js2DT(date).toFormat('dd');
 
 const timestamp = (date) => new Date(date).getTime() / 1000;
 
