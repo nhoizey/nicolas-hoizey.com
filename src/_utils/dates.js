194212fea4e0718f3741ec8afd7a7e582942ddb7
Nicolas Hoizey <nhoizey@clever-age.com>
2022-02-08 12:13:32 +0100

    Rename functions

--- src/_utils/dates.js
+++ src/_utils/dates.js
@@ -1,41 +1,44 @@
 const { DateTime } = require('luxon');
 
-const iso2DT = (date, lang = 'en') =>
+const isoToDateTime = (date, lang = 'en') =>
   DateTime.fromISO(date, { setZone: true })
     .setZone('Europe/Paris')
     .setLocale(lang);
 
-const js2DT = (date, lang = 'en') =>
+const jsToDateTime = (date, lang = 'en') =>
   DateTime.fromJSDate(date, { setZone: true })
     .setZone('Europe/Paris')
     .setLocale(lang);
 
 const formattedDate = (date, lang = 'en') =>
-  js2DT(date, lang).toFormat('d LLLL y');
+  jsToDateTime(date, lang).toFormat('d LLLL y');
 
 const formattedDateTime = (date, lang = 'en') =>
-  js2DT(date, lang).toFormat('d LLLL y, HH:mm');
+  jsToDateTime(date, lang).toFormat('d LLLL y, HH:mm');
 
 const formattedDateTimeFromIso = (date, lang = 'en') =>
-  iso2DT(date, lang).toFormat('d LLLL y, HH:mm');
+  isoToDateTime(date, lang).toFormat('d LLLL y, HH:mm');
 
 const monthString = (month, lang = 'en') => {
   // transforms "2020/02" into "February 2020"
   let fullDate = `${month.replace('/', '-')}-01T10:00:00.000Z`;
-  return iso2DT(fullDate, lang).toFormat('LLLL y');
+  return isoToDateTime(fullDate, lang).toFormat('LLLL y');
 };
 
-const dayMonth = (date, lang = 'en') => js2DT(date, lang).toFormat('LLLL y');
+const dayMonth = (date, lang = 'en') =>
+  jsToDateTime(date, lang).toFormat('LLLL y');
 
-const isoDate = (date) => js2DT(date).toISO();
+const isoDate = (date) => jsToDateTime(date).toISO();
 
-const year = (date) => js2DT(date).toFormat('y');
-const month = (date) => js2DT(date).toFormat('LL');
-const day = (date) => js2DT(date).toFormat('dd');
+const year = (date) => jsToDateTime(date).toFormat('y');
+const month = (date) => jsToDateTime(date).toFormat('LL');
+const day = (date) => jsToDateTime(date).toFormat('dd');
 
 const timestamp = (date) => new Date(date).getTime() / 1000;
 
 module.exports = {
+  isoToDateTime,
+  jsToDateTime,
   formattedDate,
   formattedDateTime,
   formattedDateTimeFromIso,
