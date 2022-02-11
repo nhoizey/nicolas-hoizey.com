dbc1752de9f20dc30fe7095be19916c05684ff22
Nicolas Hoizey <nhoizey@clever-age.com>
2022-02-11 13:32:20 +0100

    Update the time of the billet if opened multiple times

--- _scripts/journal.sh
+++ _scripts/journal.sh
@@ -18,7 +18,10 @@ timezone=$(date +"%z" | cut -b 1-3)
 folder="./src/billets/$year/$month/$day"
 billet="$folder/index.md"
 
-if [ ! -f "$billet" ]; then
+if [ -f "$billet" ]; then
+  # update the time to latest call
+  sed -i '' "s/^date: .*$/date: $year-$month-$day $hour:$minute:$second $timezone:00/" $billet
+else
   mkdir -p "$folder"
   body=""
   read -d '' body <<EOF
