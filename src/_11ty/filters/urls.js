53865cf126af4c8f9445f99f4f6e55e9c038fb84
Nicolas Hoizey <nicolas@hoizey.com>
2023-02-09 01:19:19 +0100

    Build a mapping from old to current URLs

---
+++ src/_11ty/filters/urls.js
@@ -0,0 +1,46 @@
+const memoize = require('fast-memoize');
+
+const rootUrl = require('../../../package.json').homepage;
+
+const getOldUrls = memoize((url) => {
+  url = encodeURI(url);
+  let urlsList = [];
+  if (
+    (parts = url.match(
+      /^\/articles\/([0-9]{4})\/([0-9]{2})\/([0-9]{2})\/(.*)\/$/
+    ))
+  ) {
+    // Current permalink: /articles/2018/06/15/users-do-change-font-size/
+    // /articles/2018/06/users-do-change-font-size/
+    urlsList.push(`${rootUrl}/articles/${parts[1]}/${parts[2]}/${parts[4]}/`);
+    // /2018/06/users-do-change-font-size/
+    urlsList.push(`${rootUrl}/${parts[1]}/${parts[2]}/${parts[4]}/`);
+    // /2018/06/users-do-change-font-size.html
+    urlsList.push(`${rootUrl}/${parts[1]}/${parts[2]}/${parts[4]}.html`);
+  }
+  if (
+    (parts = url.match(/^\/links\/([0-9]{4})\/([0-9]{2})\/([0-9]{2})\/(.*)\/$/))
+  ) {
+    // Current permalink: /links/2019/12/10/good-enough/
+    // /links/2019/12/good-enough/
+    urlsList.push(`${rootUrl}/links/${parts[1]}/${parts[2]}/${parts[4]}/`);
+    // /links/2019/12/good-enough.html
+    urlsList.push(`${rootUrl}/links/${parts[1]}/${parts[2]}/${parts[4]}.html`);
+  }
+  return urlsList;
+});
+
+module.exports = {
+  listUrlRewrites: (currentUrl, last = false) => {
+    const oldUrls = getOldUrls(currentUrl);
+
+    if (oldUrls.length === 0) {
+      return '';
+    }
+
+    const urlsString = oldUrls
+      .map((oldUrl) => `"${oldUrl}": "${rootUrl}${currentUrl}"`)
+      .join(',');
+    return `${urlsString}${last ? '' : ','}`;
+  },
+};
