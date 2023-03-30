622bf251a658d6f6f2b60ec0c41673d2c7d8fb5e
Nicolas Hoizey <nicolas@hoizey.com>
2023-03-31 01:07:00 +0200

    remove excerpt filter, now useless

--- src/_11ty/filters/html.js
+++ src/_11ty/filters/html.js
@@ -37,26 +37,6 @@ module.exports = {
   titleSafe: (content) => {
     return content.replace(/&#39;/g, '’');
   },
-  excerpt: (content) => {
-    if (content === undefined) {
-      return '';
-    }
-    const regex = /(<p( [^>]*)?>((?!(<\/p>)).|\n)+<\/p>)/m;
-    let excerpt = '';
-
-    // Remove paragraphs containing only an image
-    cleanContent = content.replace(/<p><img [^>]+><\/p>/, '');
-
-    // Get first paragraph, if there's at least one, and remove the paragraph tag
-    if ((matches = regex.exec(cleanContent)) !== null) {
-      excerpt = matches[0].replace(
-        /<p( [^>]*)?>(((?!(<\/p>)).|\n)+)<\/p>/,
-        '$2'
-      );
-    }
-
-    return excerpt;
-  },
   absoluteImagePath: (content, url) => {
     const HTML_IMAGE_REGEX = /<img src="([^"]+)"/g;
 
