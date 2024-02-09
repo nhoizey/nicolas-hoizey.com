342be48331412e033b4611796aa5cfca1ea73f23
Nicolas Hoizey <nicolas@hoizey.com>
2024-02-09 08:37:06 +0100

    Use prettier-plugin-jinja-template for Nunjucks

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,17 @@
 module.exports = {
+  plugins: ['prettier-plugin-jinja-template'],
+  overrides: [
+    {
+      files: '*.njk',
+      options: {
+        parser: 'jinja-template',
+      },
+    },
+  ],
   printWidth: 80,
   semi: true,
   singleQuote: true,
   tabWidth: 2,
   useTabs: false,
-  trailingComma: "es5"
+  trailingComma: 'es5',
 };
